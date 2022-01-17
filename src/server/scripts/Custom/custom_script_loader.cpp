72772df107d1a4b78ec79a15b53471d2b858b992
Elliott Quick <elliottquick@live.com>
2022-01-17 14:03:43 -0500

    Adding DK Skip Module (with additions) from master TC repo

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,10 +17,12 @@
 
 // This is where scripts' loading functions should be declared:
 void AddSC_LearnSpellsOnLevelUp();
+void AddSC_skip_StarterArea();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
     AddSC_LearnSpellsOnLevelUp();
+	AddSC_skip_StarterArea();
 }
