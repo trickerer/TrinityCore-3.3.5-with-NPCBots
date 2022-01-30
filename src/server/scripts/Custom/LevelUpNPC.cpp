4b9030fdec7c7a8a4fed37653ea69d41d5b45b0a
Elliott Quick <elliottquick@live.com>
2022-01-30 17:55:15 -0500

    Adding new profession boosting

--- src/server/scripts/Custom/LevelUpNPC.cpp
+++ src/server/scripts/Custom/LevelUpNPC.cpp
@@ -6,6 +6,7 @@
 #include "WorldSession.h"
 #include <cstring>
 #include <Cache/CharacterCache.h>
+#include <DataStores/DBCStores.cpp>
 
 // Gossip Texts
 #define GOSSIP_ITEM_LEVEL_UP_60 "Make me level 60! UwU"
@@ -18,6 +19,12 @@ enum LEVELUP_NPC_MENU_ACTIONS
     GOSSIP_ITEM_LEVEL_UP_60_OPTION = 1,
     GOSSIP_ITEM_LEVEL_UP_70_OPTION = 2,
     GOSSIP_ITEM_LEVEL_UP_80_OPTION = 3
+};
+
+enum LEVELUP_PROFESSION_STEPS {
+    MAX_LEVEL_AT_60 = 300,
+    MAX_LEVEL_AT_70 = 375,
+    MAX_LEVEL_AT_80 = 450
 };
 
 class LevelUpNPCGossip : public CreatureScript
@@ -77,10 +84,43 @@ public:
             player->GiveLevel(static_cast<uint8>(newlevel));
             player->InitTalentForLevel();
             player->SetXP(0);
+            LevelUpProfessions(player, newlevel);
         }
 
         return true;
     }
+
+    static bool LevelUpProfessions(Player* player, uint16 newPlayerLevel)
+    {
+        for (uint32 i = 0; i < sSkillLineStore.GetNumRows(); ++i)
+        {
+            SkillLineEntry const* skillInfo = sSkillLineStore.LookupEntry(i);
+            if (!skillInfo)
+                continue;
+
+            if ((skillInfo->CategoryID == SKILL_CATEGORY_PROFESSION || skillInfo->CategoryID == SKILL_CATEGORY_SECONDARY)
+                && player->GetSkillValue(skillInfo->ID) > 0)
+            {
+                uint16 maxForLevel = GetMaxProfessionLevel(newPlayerLevel);
+                player->SetSkill(skillInfo->ID, player->GetSkillStep(skillInfo->ID), maxForLevel, maxForLevel);
+                uint32 classmask = player->GetClassMask();
+            }
+        }
+        return true;
+    }
+
+    static uint16 GetMaxProfessionLevel(uint16 level)
+    {
+        switch (level)
+        {
+        case 60: return MAX_LEVEL_AT_60;
+        case 70: return MAX_LEVEL_AT_70;
+        case 80: return MAX_LEVEL_AT_80;
+        }
+
+        return 0;
+    }
+
 
     CreatureAI* GetAI(Creature* creature) const override
     {
