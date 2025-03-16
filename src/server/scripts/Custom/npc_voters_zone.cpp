e16c61c6c1305891227781637467fc12ce5f553b
biglad <biglad@mgawow.co.uk>
2025-03-16 18:59:15 +0000

    Update npc_voters_zone.cpp

--- src/server/scripts/Custom/npc_voters_zone.cpp
+++ src/server/scripts/Custom/npc_voters_zone.cpp
@@ -76,7 +76,7 @@ public:
             }
             else
             {
-                result2 = WorldDatabase.PQuery("SELECT * FROM `vote_tp` WHERE `guid`='%d' AND `time` >'%d' LIMIT 1", player->GetSession()->GetAccountId(), ttcheck);
+                result2 = WorldDatabase.PQuery("SELECT * FROM `vote_tp` WHERE `guid`='%d' AND `time` > '%d' LIMIT 1", player->GetSession()->GetAccountId(), ttcheck);
             }
             
             //result2 = WorldDatabase.PQuery("SELECT * FROM `vote_tp` WHERE `guid`='%d' AND `time` >'%d' LIMIT 1", player->GetSession()->GetAccountId(), ttcheck);
