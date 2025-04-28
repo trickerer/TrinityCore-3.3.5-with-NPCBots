e5e662972d8d9bc35d3d2325627d8c485b71802d
biglad <biglad@mgawow.co.uk>
2025-04-28 20:16:00 +0100

    typo lol

--- src/server/scripts/Custom/npc_token_shop.cpp
+++ src/server/scripts/Custom/npc_token_shop.cpp
@@ -63,7 +63,7 @@ public:
 		
 		bool UpdateReNameCharData(Player* player, int16 status)
         {
-            WorldDatabase.PExecute("UPDATE `char_rename` SET `status`='{}' WHERE `charid`='{}' AND `status` = '0'", status, pplayer->GetSession()->GetGUIDLow());
+            WorldDatabase.PExecute("UPDATE `char_rename` SET `status`='{}' WHERE `charid`='{}' AND `status` = '0'", status, player->GetSession()->GetGUIDLow());
             return true;
         }
 
