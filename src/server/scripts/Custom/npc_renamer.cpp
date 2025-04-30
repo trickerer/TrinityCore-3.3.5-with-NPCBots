310c4ba74246d9de12be24f8bcfc5554eb6585c3
biglad <biglad@mgawow.co.uk>
2025-04-30 16:26:21 +0100

    Update npc_renamer.cpp

--- src/server/scripts/Custom/npc_renamer.cpp
+++ src/server/scripts/Custom/npc_renamer.cpp
@@ -58,8 +58,8 @@ public:
 			
             WorldSession* session = player->GetSession();
 			
-			std::string s = std::to_string(player->GetSession()->GetGUIDLow());
-			WhisperTo(player, s);
+			//std::string s = std::to_string(player->GetSession()->GetGUIDLow());
+			WhisperTo(player, player->GetSession()->GetGUIDLow());
 			
 			QueryResult result;
             result = WorldDatabase.PQuery("SELECT * FROM `char_rename` WHERE `charid`='{}' AND `status`='0' LIMIT 1", player->GetSession()->GetGUIDLow());
