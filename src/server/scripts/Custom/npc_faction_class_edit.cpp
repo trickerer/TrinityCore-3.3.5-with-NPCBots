0521599ab4e5e32b20e76f9a8faf61001a716a97
biglad <biglad@mgawow.co.uk>
2025-04-30 18:31:16 +0100

    Update npc_faction_class_edit.cpp

--- src/server/scripts/Custom/npc_faction_class_edit.cpp
+++ src/server/scripts/Custom/npc_faction_class_edit.cpp
@@ -7,7 +7,7 @@
 #include "WorldDatabase.h"
 #include "DatabaseEnv.h"
 #include "WorldSession.h"
-#include "NpcBots/BotMgr.h"      // Required for GetBotMgr()
+#include "botmgr.h"    // Required for GetBotMgr()
 #include "NpcBots/bot_ai.h"    // If needed for deeper bot control
 #include <sstream>
 #include <string>
