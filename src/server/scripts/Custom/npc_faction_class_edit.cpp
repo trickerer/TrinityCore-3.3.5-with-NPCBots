c1864a35d6b0f86f1c5ce42f49f1e4c8f337def1
biglad <biglad@mgawow.co.uk>
2025-04-30 19:48:44 +0100

    Update npc_faction_class_edit.cpp

--- src/server/scripts/Custom/npc_faction_class_edit.cpp
+++ src/server/scripts/Custom/npc_faction_class_edit.cpp
@@ -94,7 +94,7 @@ public:
             return true;
         }
 
-        bool OnGossipSelect(Player* playe, uint32 /*menuId*/, uint32 gossipListId) override
+        bool OnGossipSelect(Player* player, uint32 /*menuId*/, uint32 gossipListId) override
         {
             WorldSession* session = player->GetSession();
             uint32 const action = player->PlayerTalkClass->GetGossipOptionAction(gossipListId);
