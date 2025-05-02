12f13ad568941808a59617a381749da1885cd9c1
biglad <biglad@mgawow.co.uk>
2025-05-02 17:02:31 +0100

    TEST2

--- src/server/scripts/Northrend/zone_wintergrasp.cpp
+++ src/server/scripts/Northrend/zone_wintergrasp.cpp
@@ -15,7 +15,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "AchievementCriteriaScript.h"
+#include "ScriptMgr.h"
 #include "Battlefield.h"
 #include "BattlefieldMgr.h"
 #include "BattlefieldWG.h"
