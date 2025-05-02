25cec144a61cd4c0cca35ec3e72cbc3db4b045dd
biglad <biglad@mgawow.co.uk>
2025-05-02 17:37:08 +0100

    Revert "Update zone_wintergrasp.cpp"
    This reverts commit d1d9d7d3e9f58b864d58687b36b3b0e1cad87317.

--- src/server/scripts/Northrend/zone_wintergrasp.cpp
+++ src/server/scripts/Northrend/zone_wintergrasp.cpp
@@ -68,10 +68,8 @@ enum Spells
 
 enum CreatureIds
 {
-    //NPC_GOBLIN_MECHANIC                 = 30400,
-    //NPC_GNOMISH_ENGINEER                = 30499,
-	NPC_GOBLIN_MECHANIC                 = 30499,
-	NPC_GNOMISH_ENGINEER				= 30400,
+    NPC_GOBLIN_MECHANIC                 = 30400,
+    NPC_GNOMISH_ENGINEER                = 30499,
 
     NPC_WINTERGRASP_CONTROL_ARMS        = 27852,
 
