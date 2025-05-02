a396ec2b355a9f7e3f6582d5c3d81bd3d90077d7
biglad <biglad@mgawow.co.uk>
2025-05-02 19:35:57 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -490,8 +490,22 @@ bool BattlefieldWG::SetupBattlefield()
         if (i == BATTLEFIELD_WG_WORKSHOP_SE || i == BATTLEFIELD_WG_WORKSHOP_SW)
             workshop->GiveControlTo(GetAttackerTeam(), true);
 		
+        else
+            workshop->GiveControlTo(GetDefenderTeam(), true);
+		
+
+        // Note: Capture point is added once the gameobject is created.
+        Workshops[i] = workshop;
+    }
+	
+	for (uint8 i = 0; i < WG_MAX_WORKSHOP; i++)
+    {
+        WintergraspWorkshop* workshop = new WintergraspWorkshop(this, i);	
         if (i == BATTLEFIELD_WG_WORKSHOP_NE || i == BATTLEFIELD_WG_WORKSHOP_NW)
             workshop->GiveControlTo(GetDefenderTeam(), true);
+		
+		else
+			workshop->GiveControlTo(GetAttackerTeam(), true);
 		
 
         // Note: Capture point is added once the gameobject is created.
