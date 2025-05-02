071be171baf3ae035ec9836a7e8d0f6127ee0319
biglad <biglad@mgawow.co.uk>
2025-05-02 19:51:13 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -489,7 +489,8 @@ bool BattlefieldWG::SetupBattlefield()
         WintergraspWorkshop* workshop = new WintergraspWorkshop(this, i);
         if (i == BATTLEFIELD_WG_WORKSHOP_SE || i == BATTLEFIELD_WG_WORKSHOP_SW)
             workshop->GiveControlTo(GetAttackerTeam(), true);
-        else if (i == BATTLEFIELD_WG_WORKSHOP_NE || i == BATTLEFIELD_WG_WORKSHOP_NW)
+		
+        if (i == BATTLEFIELD_WG_WORKSHOP_NE || i == BATTLEFIELD_WG_WORKSHOP_NW)
             workshop->GiveControlTo(GetDefenderTeam(), true);
 		
 
