1184e269ca7129d81af511f29f8eca622e5d2693
biglad <biglad@mgawow.co.uk>
2025-05-02 20:10:34 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -549,8 +549,9 @@ bool BattlefieldWG::Update(uint32 diff)
     bool m_return = Battlefield::Update(diff);
     if (m_saveTimer <= diff)
     {
-        sWorld->setWorldState(WS_BATTLEFIELD_WG_ACTIVE, m_isActive);
-        sWorld->setWorldState(WS_BATTLEFIELD_WG_DEFENDER, m_DefenderTeam);
+        
+		sWorld->setWorldState(WORLD_STATE_BATTLEFIELD_WG_ACTIVE, m_isActive);
+        sWorld->setWorldState(WORLD_STATE_BATTLEFIELD_WG_DEFENDER, m_DefenderTeam);
         sWorld->setWorldState(ClockWorldState[0], m_Timer);
         sWorld->setWorldState(WS_BATTLEFIELD_WG_ATTACKED_A, GetData(BATTLEFIELD_WG_DATA_WON_A));
         sWorld->setWorldState(WS_BATTLEFIELD_WG_DEFENDED_A, GetData(BATTLEFIELD_WG_DATA_DEF_A));
