e174eb88e88f44e8d1a2ceee4733952e4c6fdd5e
biglad <biglad@mgawow.co.uk>
2025-05-03 14:48:41 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -694,6 +694,11 @@ void BattlefieldWG::OnBattleEnd(bool endByTimer)
     else
         UpdateData(GetDefenderTeam() == TEAM_HORDE ? BATTLEFIELD_WG_DATA_WON_H : BATTLEFIELD_WG_DATA_WON_A, 1);
 
+    SetData(BATTLEFIELD_WG_DATA_WON_A, uint32(sWorld->getWorldState(WS_BATTLEFIELD_WG_ATTACKED_A)));
+    SetData(BATTLEFIELD_WG_DATA_DEF_A, uint32(sWorld->getWorldState(WS_BATTLEFIELD_WG_DEFENDED_A)));
+    SetData(BATTLEFIELD_WG_DATA_WON_H, uint32(sWorld->getWorldState(WS_BATTLEFIELD_WG_ATTACKED_H)));
+    SetData(BATTLEFIELD_WG_DATA_DEF_H, uint32(sWorld->getWorldState(WS_BATTLEFIELD_WG_DEFENDED_H)));
+
     // Remove turret
     for (auto itr = CanonList.begin(); itr != CanonList.end(); ++itr)
     {
