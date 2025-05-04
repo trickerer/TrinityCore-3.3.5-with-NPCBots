3c6da118c2d6d645351bbda4f4aa1177dd8ce0fb
biglad <biglad@mgawow.co.uk>
2025-05-04 13:54:04 +0100

    Update BattlefieldWG.h

--- src/server/scripts/Battlefield/BattlefieldWG.h
+++ src/server/scripts/Battlefield/BattlefieldWG.h
@@ -203,7 +203,7 @@ class WintergraspCapturePoint : public BfCapturePoint
         virtual void SetCapturePointData(GameObject* go);
         void SetTeam(TeamId team) { m_team = team; }
 
-        void ChangeTeam(TeamId oldteam) override;
+        virtual void ChangeTeam(TeamId oldteam) override;
         TeamId GetTeam() const { return m_team; }
 
     protected:
