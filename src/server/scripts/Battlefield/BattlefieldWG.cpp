6ea9eec11b81bdee72ff21f952024575dadef253
biglad <biglad@mgawow.co.uk>
2025-05-04 19:51:03 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -624,21 +624,9 @@ void BattlefieldWG::OnBattleStart()
 
 
     // Set Sliders capture points data to his owners when battle start
-    for (auto const& [id, cp] : m_capturePoints)
+    for (auto itr = m_capturePoints.begin(); itr != m_capturePoints.end(); ++itr)
     {
-        GameObject* go = cp->GetCapturePointGo();
-        if (!go)
-            continue;
-
-        uint32 entry = go->GetEntry();
-
-        // No need for reference, pass by value
-        TeamId team = (entry == GO_WINTERGRASP_FACTORY_BANNER_SE || entry == GO_WINTERGRASP_FACTORY_BANNER_SW)
-            ? GetAttackerTeam()
-            : GetDefenderTeam();
-
-        // Call with values, not references
-        cp->SetCapturePointData(go);
+        // TO DO ADD CAP POINT UPDATE SYSTEM
         SendWarning(TEST);
     }
 
