1a00dd5951e38d7564b7b4d66c827f251f93a5a1
biglad <biglad@mgawow.co.uk>
2025-05-04 15:01:46 +0100

    WG

--- src/server/scripts/Battlefield/BattlefieldWG.h
+++ src/server/scripts/Battlefield/BattlefieldWG.h
@@ -202,7 +202,7 @@ class WintergraspCapturePoint : public BfCapturePoint
         //void SetCapturePointDataWithTeam(GameObject* go, TeamId team);
         //void SetTeam(TeamId team) { m_team = team; }
 
-        void SetCapturePointData(GameObject*);  // or just declared, not implemented
+        //void SetCapturePointData(GameObject*);  // or just declared, not implemented
 
         void ChangeTeam(TeamId oldteam) override;
         TeamId GetTeam() const { return m_team; }
