2d7c41ecfd90ed47e5f8fcf81ece694a74267756
biglad <biglad@mgawow.co.uk>
2025-05-04 17:56:52 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -548,40 +548,6 @@ bool BattlefieldWG::SetupBattlefield()
         }
     }
 
-    for (uint8 i = 0; i < WG_MAX_WORKSHOP; i++)
-    {
-        WintergraspWorkshop* workshop = new WintergraspWorkshop(this, i);
-        if (i == BATTLEFIELD_WG_WORKSHOP_NE || i == BATTLEFIELD_WG_WORKSHOP_NW)
-            workshop->GiveControlTo(GetAttackerTeam(), true);
-        else
-            workshop->GiveControlTo(GetDefenderTeam(), true);
-
-        // Note: Capture point is added once the gameobject is created.
-        Workshops[i] = workshop;
-    }
-
-    for (uint8 i = 0; i < WG_MAX_WORKSHOP; i++)
-    {
-        WintergraspWorkshop* workshop = new WintergraspWorkshop(this, i);
-        if (i == BATTLEFIELD_WG_WORKSHOP_SE || i == BATTLEFIELD_WG_WORKSHOP_SW)
-            workshop->GiveControlTo(GetDefenderTeam(), true);
-        else
-            workshop->GiveControlTo(GetAttackerTeam(), true);
-
-        // Note: Capture point is added once the gameobject is created.
-        Workshops[i] = workshop;
-    }
-
-    for (uint8 i = 0; i < WG_MAX_WORKSHOP; i++)
-    {
-        WintergraspWorkshop* workshop = new WintergraspWorkshop(this, i);
-        if (i == BATTLEFIELD_WG_WORKSHOP_KEEP_WEST || i == BATTLEFIELD_WG_WORKSHOP_KEEP_EAST)
-            workshop->GiveControlTo(GetAttackerTeam(), true);
-
-        // Note: Capture point is added once the gameobject is created.
-        Workshops[i] = workshop;
-    }
-
     UpdateCounterVehicle(true);
     return true;
 }
@@ -892,6 +858,7 @@ void BattlefieldWG::OnBattleEnd(bool endByTimer)
             HideNpc(creature);
         }
     }
+
 
     for (WintergraspWorkshop* workshop : Workshops)
         workshop->UpdateGraveyardAndWorkshop();
