c4894eff4fcb09f53f905c2496b5ca4f02a01d27
biglad <biglad@mgawow.co.uk>
2025-05-10 00:47:07 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -1,7 +1,5 @@
-#include "ScriptMgr.h"
-#include "Config.h"
-#include "Log.h"
-#include "Player.h"
+// Workaround for GCC 13: avoid ambiguity between std::format and Poco::format
+#define format __poco_format_workaround
 #include <Poco/Net/HTTPClientSession.h>
 #include <Poco/Net/HTTPSClientSession.h>
 #include <Poco/Net/HTTPRequest.h>
@@ -9,6 +7,14 @@
 #include <Poco/URI.h>
 #include <Poco/StreamCopier.h>
 #include <Poco/Exception.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Stringifier.h>
+#undef format
+
+// TrinityCore headers
+#include "ScriptMgr.h"
+#include "Config.h"
+#include "Log.h"
 
 class DiscordWebhookPlayerActivity : public PlayerScript
 {
