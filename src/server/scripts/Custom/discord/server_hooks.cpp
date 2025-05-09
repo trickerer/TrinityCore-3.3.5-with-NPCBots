bc9d3466c0f0e124c52cb7bfaec6a278ca55a69c
biglad <biglad@mgawow.co.uk>
2025-05-09 18:59:05 +0100

    Update server_hooks.cpp

--- src/server/scripts/Custom/discord/server_hooks.cpp
+++ src/server/scripts/Custom/discord/server_hooks.cpp
@@ -13,18 +13,18 @@ public:
         //std::string webhookUrl = "https://discord.com/api/webhooks/1317301859865726977/Qs9DOX26Lh89rQe8zXyDApj7dLz6QijPMyB_gSSSsGBVx7cJgZZ7Fdn_-NeiYvzR34Fh";
         std::string webhookUrl = sConfigMgr->GetOption<std::string>("Webhook.URL", "");
         if (std::empty(webhookUrl)) {
-            LOG_ERROR("server.worldserver", "Webhook url is empty. Disabling module. Please provide a valid url.");
+            TC_LOG_ERROR("server.worldserver", "Webhook url is empty. Disabling module. Please provide a valid url.");
             return;
         }
 
-        LOG_INFO("server.worldserver", ">> Webhook module initialized.");
+        TC_LOG_INFO("server.worldserver", ">> Webhook module initialized.");
         sWebhookMgr->SetWebhookUrl(webhookUrl);
         sWebhookMgr->Start();
     }
 
     void OnShutdown() override
     {
-        LOG_INFO("server.worldserver", "Stopping webhook queue...");
+        TC_LOG_INFO("server.worldserver", "Stopping webhook queue...");
         sWebhookMgr->Stop();
         return;
     }
