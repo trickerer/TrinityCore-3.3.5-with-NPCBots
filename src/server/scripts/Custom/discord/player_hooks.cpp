bb58ad5d18ea4512ab793e1bc6821d41531f833d
biglad <biglad@mgawow.co.uk>
2025-05-10 00:05:06 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -15,6 +15,7 @@
 #include "ScriptMgr.h"
 #include "Config.h"
 #include "Log.h"
+#include "PlayerScript.h"
 
 // Inherit from PlayerScript (which is already part of TrinityCore)
 class DiscordWebhookPlayerActivity : public PlayerScript
