24221c8befa65a6fa76e806cef736c2268fb9581
biglad <biglad@mgawow.co.uk>
2025-05-09 23:55:56 +0100

    Update custom_script_loader.cpp

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -33,8 +33,8 @@ void AddSC_npc_xp_mod();
 void AddSC_npc_insta_80();
 void AddSC_npc_dungeon_master();
 void AddDiscordWebhookServerHookScripts();
-void AddDiscordWebhookPlayerLoginScripts();
-
+void AddDiscordWebhookPlayerLoginScripts(); 
+ 
 void AddCustomScripts()
 {
 	AddSC_npc_voters_zone();
