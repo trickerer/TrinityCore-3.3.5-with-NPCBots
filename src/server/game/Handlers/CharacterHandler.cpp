a07875901af2e14269c66e31baf3f34f7a5c3657
biglad <biglad@mgawow.co.uk>
2025-05-10 14:18:08 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -808,8 +808,8 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     SendPacket(&data);
 
     pCurrChar->SendInitialPacketsBeforeAddToMap();
-	sScriptMgr->OnLogin(pCurrChar);
-	//m_player = pCurrChar;
+	for (auto const& script : sScriptMgr->GetPlayerScripts())
+		script->OnLogin(pCurrChar);
 
     //Show cinematic at the first time that player login
     if (!pCurrChar->getCinematic())
