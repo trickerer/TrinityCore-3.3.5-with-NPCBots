2b7ae4baeba3cde4588b6877d805e47ea89b3888
biglad <biglad@mgawow.co.uk>
2025-05-11 20:12:00 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -49,17 +49,17 @@ public:
     void OnLogout(Player* player)
 	{
 		if (serverShuttingDown)
-    {
-        std::string message = "All players have been logged out..";
-        std::string webhookUrl = sConfigMgr->GetStringDefault("Webhook.URL", "");
-        if (!webhookUrl.empty())
-        {
-            SendDiscordWebhook(webhookUrl, message);  // You need to call the SendDiscordWebhook function
-        }
+		{
+			std::string message = "All players have been logged out..";
+			std::string webhookUrl = sConfigMgr->GetStringDefault("Webhook.URL", "");
+			if (!webhookUrl.empty())
+			{
+				SendDiscordWebhook(webhookUrl, message);  // You need to call the SendDiscordWebhook function
+			}
 
-        // Prevent further logout actions when the server is shutting down
-        return;
-    }
+			// Prevent further logout actions when the server is shutting down
+			return;
+		}
 		uint64 guid = player->GetGUID();
 		LoggedInGuids.erase(guid);
 
