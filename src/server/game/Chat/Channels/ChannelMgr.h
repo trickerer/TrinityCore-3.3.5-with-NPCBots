a696ace6195250338a717b4ad18e08930dcf4cfe
biglad <biglad@mgawow.co.uk>
2025-05-12 20:58:49 +0100

    Update ChannelMgr.h

--- src/server/game/Chat/Channels/ChannelMgr.h
+++ src/server/game/Chat/Channels/ChannelMgr.h
@@ -40,6 +40,7 @@ class TC_GAME_API ChannelMgr
         static void LoadFromDB();
         static ChannelMgr* forTeam(uint32 team);
         static Channel* GetChannelForPlayerByNamePart(std::string const& namePart, Player* playerSearcher);
+        void MakeNotOnPacket(WorldPacket* data, std::string const& name);
 
         void SaveToDB();
         Channel* GetSystemChannel(uint32 channelId, AreaTableEntry const* zoneEntry = nullptr);
