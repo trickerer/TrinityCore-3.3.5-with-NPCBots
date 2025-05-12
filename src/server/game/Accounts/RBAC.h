2ae78ee4d14d5e39bd9f88a21306ecd3032d1cf7
biglad <biglad@mgawow.co.uk>
2025-05-12 17:50:10 +0100

    Update RBAC.h

--- src/server/game/Accounts/RBAC.h
+++ src/server/game/Accounts/RBAC.h
@@ -791,6 +791,7 @@ enum RBACPermissions
     RBAC_PERM_COMMAND_NPCBOT_CREATENEW                       = 70036,
     RBAC_PERM_COMMAND_NPCBOT_SEND                            = 70037,
     //End NPCBot
+	RBAC_PERM_COMMAND_SENDWORLD 							 = 80000,
     RBAC_PERM_MAX
 };
 
