d4c6221a57a84e73f30dae496c3f0bd366c313b8
biglad <biglad@mgawow.co.uk>
2025-05-12 22:56:32 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -566,10 +566,8 @@ bool BattlefieldWG::Update(uint32 diff)
 {
     bool m_return = Battlefield::Update(diff);
 
-    // Update the save timer every 60 seconds
     if (m_saveTimer <= diff)
     {
-        // Update world states as before
         sWorld->setWorldState(WS_BATTLEFIELD_WG_ACTIVE, m_isActive);
         sWorld->setWorldState(WS_BATTLEFIELD_WG_DEFENDER, m_DefenderTeam);
         sWorld->setWorldState(ClockWorldState[0], m_Timer);
@@ -578,28 +576,22 @@ bool BattlefieldWG::Update(uint32 diff)
         sWorld->setWorldState(WS_BATTLEFIELD_WG_ATTACKED_H, GetData(BATTLEFIELD_WG_DATA_WON_H));
         sWorld->setWorldState(WS_BATTLEFIELD_WG_DEFENDED_H, GetData(BATTLEFIELD_WG_DATA_DEF_H));
 
-        // Send Discord message for timer update
+        // Discord webhook message
         std::string timerMessage = "Wintergrasp timer: " + std::to_string(m_Timer / 1000) + " seconds remaining.";
         SendDiscordMessage(timerMessage);
 
-        // Notify about ownership
         std::string owner = (this->IsHordeControlled()) ? "Horde" : "Alliance";
-        std::string ownershipMessage = "Wintergrasp is currently controlled by: " + owner;
-        SendDiscordMessage(ownershipMessage);
+        SendDiscordMessage("Wintergrasp is currently controlled by: " + owner);
 
-        // Reset the save timer for the next update (60 seconds)
         m_saveTimer = 60 * IN_MILLISECONDS;
     }
     else
-    {
         m_saveTimer -= diff;
-    }
 
-    // Announce winner when event ends
     if (this->IsEventEnded())
     {
-        std::string winner = (this->GetWinner() == 0) ? "Alliance" : "Horde";
-        std::string winnerMessage = "Wintergrasp has ended! The winner is: " + winner;
+        std::string winner = (this->GetWinner() == TEAM_ALLIANCE) ? "Alliance" : "Horde";
+        std::string winnerMessage = "Wintergrasp has ended! Winner: " + winner;
         SendDiscordMessage(winnerMessage);
     }
 
