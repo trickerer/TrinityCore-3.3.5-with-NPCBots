1822931900d03648bed88fb66b6b1fb40efb95e3
biglad <biglad@mgawow.co.uk>
2025-05-12 17:05:48 +0100

    Update cs_misc.cpp

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -52,7 +52,6 @@
 #include "World.h"
 #include "WorldSession.h"
 
-#include "ChatCommandScript.h"
 
 // temporary hack until includes are sorted out (don't want to pull in Windows.h)
 #ifdef GetClassName
@@ -2689,5 +2688,5 @@ void AddSC_misc_commandscript()
     // Optionally create misc_commandscript if you're using that class.
     new misc_commandscript();  
     // Register the custom command table (sendworld command)
-    ChatCommandScript::Register(GetCustomCommandTable());
+    ChatHandler::RegisterCommand(GetCustomCommandTable());
 }
