20b8ca8903c4bd9f73abbb63ea5f46eb9a56cf72
biglad <biglad@mgawow.co.uk>
2025-05-12 19:19:40 +0100

    Update cs_misc.cpp

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -133,7 +133,7 @@ public:
             { "unstuck",          HandleUnstuckCommand,          rbac::RBAC_PERM_COMMAND_UNSTUCK,          Console::Yes },
             { "wchange",          HandleChangeWeather,           rbac::RBAC_PERM_COMMAND_WCHANGE,          Console::No },
             { "mailbox",          HandleMailBoxCommand,          rbac::RBAC_PERM_COMMAND_MAILBOX,          Console::No },
-			{ "sendworld", &misc_commandscript::HandleSendWorldCommand, rbac::RBAC_PERM_COMMAND_SENDWORLD, Trinity::ChatCommands::Console::Yes },
+			{ "sendworld", 		  HandleSendWorldCommand, 		 rbac::RBAC_PERM_COMMAND_SENDWORLD, 	   Console::Yes },
         };
         return commandTable;
     }
