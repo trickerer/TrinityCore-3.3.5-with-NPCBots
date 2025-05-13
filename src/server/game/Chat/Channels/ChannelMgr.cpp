495126b005f03278744b073fae688eb5335d74af
biglad <biglad@mgawow.co.uk>
2025-05-13 19:09:15 +0100

    Update ChannelMgr.cpp

--- src/server/game/Chat/Channels/ChannelMgr.cpp
+++ src/server/game/Chat/Channels/ChannelMgr.cpp
@@ -168,6 +168,12 @@ Channel* ChannelMgr::GetSystemChannel(uint32 channelId, AreaTableEntry const* zo
     return newChannel;
 }
 
+Channel* worldChannel = ChannelMgr::GetChannelByName("world");
+if (!worldChannel)
+{
+    worldChannel = ChannelMgr::CreateCustomChannel("world");
+}
+
 Channel* ChannelMgr::CreateCustomChannel(std::string const& name)
 {
     std::wstring channelName;
