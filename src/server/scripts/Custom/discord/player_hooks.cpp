9d0b28ddbe15a6e60e9fa043a75840637fb26d5d
biglad <biglad@mgawow.co.uk>
2025-05-13 22:46:29 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -79,7 +79,7 @@ private:
 
         std::string name = player->GetName();
         std::string gmTag = player->GetSession()->GetSecurity() > SEC_PLAYER ? "🛡️ " : "";
-        std::string status = loggingIn ? "🟢 **Logged In**" : "🔴 **Logged Out**";
+        std::string status = loggingIn ? "🟢 **Logged In**" : "🛑 **Logged Out**";
 
         std::ostringstream messageStream;
         messageStream << gmTag << "**Player " << status << "**\nName: `" << name << "`";
