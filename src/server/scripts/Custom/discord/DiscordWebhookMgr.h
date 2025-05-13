94036c41e4f43cc9ee4263d4df38dfc9bb2a6af7
biglad <biglad@mgawow.co.uk>
2025-05-13 13:23:20 +0100

    lol

---
+++ src/server/scripts/Custom/discord/DiscordWebhookMgr.h
@@ -0,0 +1,9 @@
+#ifndef DISCORD_WEBHOOK_MGR_H
+#define DISCORD_WEBHOOK_MGR_H
+
+#include <string>
+
+void SendDiscordMessage(const std::string& message);
+void SendBattlegroundDiscordWebhook(const std::string& webhookUrl, const std::string& battlegroundName, uint32 alliancePlayers, uint32 hordePlayers);
+
+#endif // DISCORD_WEBHOOK_MGR_H
