0ccfc51acc08f4a664b179056b924b05a59133f8
biglad <biglad@mgawow.co.uk>
2025-05-14 20:49:50 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -18,7 +18,7 @@ public:
         uint64 guid = player->GetGUID();
         uint32 now = time(nullptr);
         
-        sLog->outString("Test message: item_aaron_summon used by player GUID: %llu", guid);
+        TC_LOG_INFO("player.hooks", "Test message: item_aaron_summon used by player GUID: %llu", guid);
         // 1800 = 30 minutes
         if (lastUsedTime.count(guid) && now - lastUsedTime[guid] < 1800)
         {
