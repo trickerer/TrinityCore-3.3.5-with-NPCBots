fc1772eabdeb70a1261802989cc7ed375655ef26
biglad <biglad@mgawow.co.uk>
2025-05-14 22:06:38 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -2,21 +2,18 @@
 #include "Player.h"
 #include "Creature.h"
 #include "WorldSession.h"
-#include "Map.h"
 #include "Log.h"
+#include "SpellMgr.h"
 
 class item_aaron_summon : public ItemScript
 {
 public:
-    item_aaron_summon() : ItemScript("item_aaron_summon") 
-    {
-        TC_LOG_INFO("player.hooks", "Loaded item_aaron_summon script.");
-    }
+    item_aaron_summon() : ItemScript("item_aaron_summon") { }
 
-    bool OnUse(Player* player, Item* /*item*/, SpellCastTargets const& /*targets*/) override
+    bool OnUse(Player* player, Item* item, SpellCastTargets const& /*targets*/) override
     {
-        TC_LOG_INFO("player.hooks", "item_aaron_summon OnUse triggered!");
-        player->Say("Item worked!", LANG_UNIVERSAL);
+        TC_LOG_INFO("player.hooks", "AARON ITEM USED BY: %s", player->GetName().c_str());
+        player->Say("IT WORKED!!", LANG_UNIVERSAL);
         return true;
     }
 };
