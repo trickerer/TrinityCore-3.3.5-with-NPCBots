99b9b9bff69923ad70fafd9dc6ff723d32e102ed
biglad <biglad@mgawow.co.uk>
2025-05-14 23:25:14 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -5,6 +5,8 @@
 #include "Log.h"
 #include "SpellMgr.h"
 #include "Item.h"
+#include "ItemTemplate.h"
+#include "ObjectAccessor.h"
 
 class item_aaron_summon : public ItemScript
 {
@@ -16,8 +18,6 @@ public:
     bool OnUse(Player* player, Item* /*item*/, SpellCastTargets const& /*targets*/) override
     {
         uint64 guid = player->GetGUID();
-        //TC_LOG_INFO("player.hooks", "Item used by player GUID: %llu", guid);
-
 
         // Implement cooldown logic to prevent abuse (30 minutes cooldown)
         uint32 now = time(nullptr);
@@ -28,29 +28,29 @@ public:
             uint32 remainingSeconds = remaining % 60;
             
             player->GetSession()->SendNotification("You must wait %u minute(s) and %u second(s) to use this item again.", remainingMinutes, remainingSeconds);
-            return false;
+            return false;  // Do not continue if item is on cooldown
         }
 
         lastUsedTime[guid] = now; // Update last used time
 
-        // If the item can be used, proceed to summon a creature
-        uint32 creatureId = 500612; // Replace with your custom creature ID
+        // If the item can be used, proceed to summon a creature (Aaron)
+        uint32 creatureId = 500612; // Replace with your custom creature ID (Aaron)
         float x, y, z;
         player->GetPosition(x, y, z); // Get player's current position
 
-        // Define position offset for summoned creature (2 yards away from the player)
-        float orientation = player->GetOrientation() + M_PI;
-        float distance = -2.0f; // 2 yards in front
+        // Calculate position to spawn the creature directly in front of the player (facing direction)
+        float orientation = player->GetOrientation() + M_PI; // Reverse the orientation (to be in front)
+        float distance = -2.0f; // 2 yards in front of the player
 
+        // Position calculation
         float spawnX = x + distance * std::cos(orientation);
         float spawnY = y + distance * std::sin(orientation);
+        Position pos(spawnX, spawnY, z, orientation);
 
-Position pos(spawnX, spawnY, z, orientation);
-        
         // Summon the creature
         Creature* summon = player->GetMap()->SummonCreature(
             creatureId,              // Creature ID
-            pos,                     // Position to summon at
+            pos,                     // Position to spawn at
             nullptr,                 // No custom summon properties
             2 * MINUTE * IN_MILLISECONDS, // Creature will despawn in 2 minutes
             player,                  // Player who summoned it
@@ -69,8 +69,65 @@ Position pos(spawnX, spawnY, z, orientation);
     }
 };
 
-// Function to add the script to the server
+// Temporary Mailbox item (30-minute cooldown)
+class item_temp_mailbox : public ItemScript
+{
+public:
+    item_temp_mailbox() : ItemScript("item_temp_mailbox") { }
+
+    std::unordered_map<uint64, uint32> lastUsedTime;
+
+    bool OnUse(Player* player, Item* /*item*/, SpellCastTargets const& /*targets*/) override
+    {
+        uint64 guid = player->GetGUID();
+
+        // Implement cooldown logic to prevent abuse (30 minutes cooldown)
+        uint32 now = time(nullptr);
+        if (lastUsedTime.count(guid) && now - lastUsedTime[guid] < 1800) // 1800 seconds = 30 minutes
+        {
+            uint32 remaining = 1800 - (now - lastUsedTime[guid]);
+            uint32 remainingMinutes = remaining / 60;
+            uint32 remainingSeconds = remaining % 60;
+            
+            player->GetSession()->SendNotification("You must wait %u minute(s) and %u second(s) to use this item again.", remainingMinutes, remainingSeconds);
+            return false;  // Do not continue if item is on cooldown
+        }
+
+        lastUsedTime[guid] = now; // Update last used time
+
+        // Spawn the temporary mailbox
+        float x, y, z;
+        player->GetPosition(x, y, z); // Get player's current position
+
+        // Position mailbox in front of the player (2 yards)
+        float orientation = player->GetOrientation();
+        float distance = 2.0f; // 2 yards in front
+
+        float spawnX = x + distance * std::cos(orientation);
+        float spawnY = y + distance * std::sin(orientation);
+        Position pos(spawnX, spawnY, z, orientation);
+
+        // Summon the mailbox creature
+        uint32 mailboxId = 190000; // Replace with the mailbox creature ID (ensure this ID is valid)
+        Creature* mailbox = player->GetMap()->SummonCreature(mailboxId, pos, nullptr, 5 * MINUTE * IN_MILLISECONDS, player); // Mailbox will disappear after 5 minutes
+
+        if (mailbox)
+        {
+            mailbox->SetOrientation(player->GetOrientation());  // Set mailbox to face the player
+            player->SendBroadcastMessage("A temporary mailbox has been summoned for you. It will disappear in 5 minutes.");
+        }
+
+        return true;  // Item use was successful
+    }
+};
+
+// Register scripts
 void AddSC_item_aaron_summon()
 {
-    new item_aaron_summon(); // Register the item script
+    new item_aaron_summon(); // Register the Aaron summon item script
+}
+
+void AddSC_item_temp_mailbox()
+{
+    new item_temp_mailbox(); // Register the temporary mailbox item script
 }
