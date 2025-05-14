4eae3de9774944c3d44b9fe67a9d7feed44691da
biglad <biglad@mgawow.co.uk>
2025-05-14 13:27:59 +0100

    Update DiscordWebhookMgr.h

--- src/server/scripts/Custom/discord/DiscordWebhookMgr.h
+++ src/server/scripts/Custom/discord/DiscordWebhookMgr.h
@@ -4,6 +4,6 @@
 #include <string>
 
 void SendDiscordMessage(const std::string& message);
-void SendBattlegroundDiscordWebhook(const std::string& webhookUrl, const std::string& battlegroundName, uint32 alliancePlayers, uint32 hordePlayers);
+void SendBattlegroundDiscordWebhook(const std::string& webhookUrl, const std::string& battlegroundName, uint32 alliancePlayers, uint32 hordePlayers, const std::string& bracket);
 
 #endif // DISCORD_WEBHOOK_MGR_H
