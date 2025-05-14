ca39c1c9f5f438d8e0b81c476097f8b33a35c831
biglad <biglad@mgawow.co.uk>
2025-05-14 22:31:32 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -28,7 +28,7 @@ public:
             uint32 remainingSeconds = remaining % 60;
             
             player->GetSession()->SendNotification("You must wait %u minute(s) and %u second(s) to use this item again.", remainingMinutes, remainingSeconds);
-            item->SetSpellId(0);
+            Item->SetSpellId(0);
             return false;
         }
 
