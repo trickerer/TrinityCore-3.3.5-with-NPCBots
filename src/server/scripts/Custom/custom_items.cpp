bb8fe585a32c01085e60547d7e4172a4e5cefe7c
biglad <biglad@mgawow.co.uk>
2025-05-15 00:21:23 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -141,12 +141,8 @@ public:
 };
 
 // Register scripts
-void AddSC_item_aaron_summon()
+void AddSC_custom_items()
 {
-    new item_aaron_summon(); // Register the Aaron summon item script
-}
-
-void AddSC_item_temp_mailbox()
-{
-    new item_temp_mailbox(); // Register the temporary mailbox item script
+    AddSC_item_aaron_summon();
+    AddSC_item_temp_mailbox();
 }
