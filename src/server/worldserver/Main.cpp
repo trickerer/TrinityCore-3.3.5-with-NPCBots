1e169f04309bc7f4d5104be2632ed252a7fe0fcd
biglad <biglad@mgawow.co.uk>
2025-05-15 17:38:39 +0100

    Update Main.cpp

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -433,7 +433,6 @@ extern int main(int argc, char** argv)
     // 2 - restart command used, this code can be used by restarter for restart Trinityd
     
     Poco::Net::uninitializeSSL();
-    Poco::Net::SSLManager::instance().shutdown();
     return World::GetExitCode();
 }
 
