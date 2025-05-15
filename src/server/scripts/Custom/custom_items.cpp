af6dff52691910cfbe11a1263a967a9665768270
biglad <biglad@mgawow.co.uk>
2025-05-15 16:34:41 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -78,9 +78,23 @@ class item_temp_mailbox : public ItemScript
 {
 public:
     item_temp_mailbox() : ItemScript("item_temp_mailbox") { }
+    
+    std::unordered_map<uint64, uint32> lastUsedTime2;
 
     bool OnUse(Player* player, Item* /*item*/, SpellCastTargets const& /*targets*/) override
     {
+        // Implement cooldown logic to prevent abuse (30 minutes cooldown)
+        uint32 now = time(nullptr);
+        if (lastUsedTime2.count(guid) && now - lastUsedTime2[guid] < 1800) // 1800 seconds = 30 minutes
+        {
+            uint32 remaining = 1800 - (now - lastUsedTime2[guid]);
+            uint32 remainingMinutes = remaining / 60;
+            uint32 remainingSeconds = remaining % 60;
+            
+            player->GetSession()->SendNotification("You must wait %u minute(s) and %u second(s) to use this item again.", remainingMinutes, remainingSeconds);
+            return false;  // Do not continue if item is on cooldown
+        }
+        
         float x, y, z;
         player->GetPosition(x, y, z);
 
