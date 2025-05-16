f43ce88f29c220f77900292bf5e02f7bce1349e7
biglad <biglad@mgawow.co.uk>
2025-05-16 16:22:10 +0100

    Update npc_voters_zone.cpp

--- src/server/scripts/Custom/npc_voters_zone.cpp
+++ src/server/scripts/Custom/npc_voters_zone.cpp
@@ -86,10 +86,10 @@ public:
            
             if (result2)
             {
-                AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO_TP1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1000);
-				AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO_TP4, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1003);
+                AddGossipItemFor(player, GOSSIP_ICON_TAXI, GOSSIP_HELLO_TP1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1000);
+				AddGossipItemFor(player, GOSSIP_ICON_TAXI, GOSSIP_HELLO_TP4, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1003);
                 AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO_TP3, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1001);
-                AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_OPTION_OPEN_GBANK, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1004);
+                AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_OPTION_OPEN_GBANK, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1004);
                 //if (player->GetGuild())
                 //{
                 //    AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_OPTION_OPEN_BANK, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1005);
@@ -97,10 +97,10 @@ public:
             }
             else if (player->IsGameMaster())
             {
-                AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO_TP1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1000);
-                AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO_TP4, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1003);
+                AddGossipItemFor(player, GOSSIP_ICON_TAXI, GOSSIP_HELLO_TP1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1000);
+                AddGossipItemFor(player, GOSSIP_ICON_TAXI, GOSSIP_HELLO_TP4, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1003);
                 AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO_TP3, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1001);
-                AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_OPTION_OPEN_GBANK, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1004);
+                AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_OPTION_OPEN_GBANK, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1004);
                 //if (player->GetGuild())
                 //{
                 //    AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_OPTION_OPEN_BANK, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1005);
