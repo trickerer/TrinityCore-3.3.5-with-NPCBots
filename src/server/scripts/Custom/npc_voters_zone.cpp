22e62ac7a6e69d573ba627662bb24ba5f3a81c33
biglad <biglad@mgawow.co.uk>
2025-05-16 13:51:48 +0100

    updates to scripts

--- src/server/scripts/Custom/npc_voters_zone.cpp
+++ src/server/scripts/Custom/npc_voters_zone.cpp
@@ -180,7 +180,8 @@ public:
 			if (action == GOSSIP_ACTION_INFO_DEF + 1003)
 			{
 				CloseGossipMenuFor(player);
-				player->CastSpell(player, 8690, true); //https://www.wowhead.com/wotlk/spell=8690/hearthstone
+                player->TeleportTo(player->m_homebindMapId, player->m_homebindX, player->m_homebindY, player->m_homebindZ, 0.0f);
+				//player->CastSpell(player, 8690, true); //https://www.wowhead.com/wotlk/spell=8690/hearthstone
 				player->SetPvP(false);
 				return true;
 			}
