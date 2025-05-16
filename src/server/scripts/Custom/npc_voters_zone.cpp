cdf7eb16844ffc15ceefab083f56ef3757f6cc14
biglad <biglad@mgawow.co.uk>
2025-05-16 16:55:19 +0100

    Update npc_voters_zone.cpp

--- src/server/scripts/Custom/npc_voters_zone.cpp
+++ src/server/scripts/Custom/npc_voters_zone.cpp
@@ -57,7 +57,7 @@ public:
             me->Whisper(message, LANG_UNIVERSAL, player);
         }
 
-        bool OnGossipHello(Player* player, Creature* creature) override
+        bool OnGossipHello(Player* player, Creature* creature) //override
         {
             me->HandleEmoteCommand(EMOTE_ONESHOT_WAVE);
             InitGossipMenuFor(player, NPC_GOSSIP_MENU);
@@ -124,7 +124,7 @@ public:
             return true;
         }
 
-        bool OnGossipSelect(Player* player, Creature* creature, uint32 /*menuId*/, uint32 gossipListId) override
+        bool OnGossipSelect(Player* player, Creature* creature, uint32 /*menuId*/, uint32 gossipListId) //override
         {
             uint32 const action = player->PlayerTalkClass->GetGossipOptionAction(gossipListId);
             ClearGossipMenuFor(player);
