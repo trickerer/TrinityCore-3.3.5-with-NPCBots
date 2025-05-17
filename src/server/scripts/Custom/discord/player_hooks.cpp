96e450157b30de566ec0761a4b4eb09ef856227b
biglad <biglad@mgawow.co.uk>
2025-05-17 14:56:36 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -35,6 +35,16 @@ public:
 	}
     void OnAchievementEarned(Player* player, AchievementEntry const* achievement) //override
     {
+        
+        static std::string GetLocalizedAchievementName(uint32 id)
+        {
+            const AchievementEntry* achievement = sAchievementStore.LookupEntry(id);
+            if (!achievement)
+                return "Unknown Achievement";
+
+            return achievement->name[sWorld->GetDefaultDbcLocale()];
+        }
+        
         std::string webhookUrl = sConfigMgr->GetStringDefault("Webhook.URL", "");
         if (webhookUrl.empty())
         {
