ea277ad9a7a2096cbb5e53c2621b7255224d6252
biglad <biglad@mgawow.co.uk>
2025-05-17 19:21:53 +0100

    Update AchievementMgr.cpp

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -1587,10 +1587,14 @@ void AchievementMgr::CompletedAchievement(AchievementEntry const* achievement)
     }
     if (m_player)
     {
-        TC_LOG_INFO("achievement", "Calling OnAchievementEarned for player: {}", m_player->GetName());
-        sScriptMgr->OnAchievementEarned(m_player, achievement);
+        //TC_LOG_INFO("achievement", "Calling OnAchievementEarned for player: {}", m_player->GetName());
+        //sScriptMgr->OnAchievementEarned(m_player, achievement);
+
+        // Add Discord webhook message here:
+        std::ostringstream msg;
+        msg << "**" << m_player->GetName() << "** earned achievement: **" << achievement->name[0] << "** 🏆";
+        SendDiscordWebhook(msg.str());
     }
-}
 
 void AchievementMgr::SendAllAchievementData() const
 {
