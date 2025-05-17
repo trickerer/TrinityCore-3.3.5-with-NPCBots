eddddae31b3f4b5f33741938a3553a9386cb3ef8
biglad <biglad@mgawow.co.uk>
2025-05-17 14:52:34 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -32,7 +32,7 @@ public:
 	{
 		TC_LOG_INFO("player.hooks", "DiscordWebhookPlayerActivity script loaded.");
 	}
-    void OnAchievementEarned(Player* player, AchievementEntry const* achievement) override
+    void OnAchievementEarned(Player* player, AchievementEntry const* achievement) //override
     {
         std::string webhookUrl = sConfigMgr->GetStringDefault("Webhook.URL", "");
         if (webhookUrl.empty())
