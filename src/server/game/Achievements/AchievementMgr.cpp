faf6c9fe798c23c5c9185410eaceeb9041437111
biglad <biglad@mgawow.co.uk>
2025-05-18 18:31:27 +0100

    Update AchievementMgr.cpp

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -1529,6 +1529,9 @@ void AchievementMgr::CompletedAchievement(AchievementEntry const* achievement)
     // Disable for GameMasters with GM-mode enabled or for players that don't have the related RBAC permission
     if (m_player->IsGameMaster() || m_player->GetSession()->HasPermission(rbac::RBAC_PERM_CANNOT_EARN_ACHIEVEMENTS))
         return;
+    
+    //if (achievement->Flags & ACHIEVEMENT_FLAG_COUNTER || HasAchieved(achievement->ID))
+    //    return;
  
     if (HasAchieved(achievement->ID))
         return;
