3ae4f47d58dd498bb2589947d430be171e9fe5c1
biglad <biglad@mgawow.co.uk>
2025-05-21 22:47:37 +0100

    Update custom_items.cpp

--- src/server/scripts/Custom/custom_items.cpp
+++ src/server/scripts/Custom/custom_items.cpp
@@ -275,7 +275,7 @@ void AddSC_item_temp_gvault()
     new item_temp_gvault(); // Register the temporary gvault item script
 }
 
-void ItemAuraVisualScript()
+void AddSC_ItemAuraVisualScript()
 {
     new ItemAuraVisualScript();
 }
