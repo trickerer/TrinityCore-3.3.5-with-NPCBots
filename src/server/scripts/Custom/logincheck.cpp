c5053337ed18bfee66ac3ae5a663944619d90909
biglad <biglad@mgawow.co.uk>
2025-05-22 12:18:27 +0100

    Update logincheck.cpp

--- src/server/scripts/Custom/logincheck.cpp
+++ src/server/scripts/Custom/logincheck.cpp
@@ -10,8 +10,8 @@ public:
 
     void OnLogin(Player* player, bool /*firstLogin*/)
     {
-        ChatHandler(player->GetSession()).PSendSysMessage(">> TEST: Login hook triggered.");
-        TC_LOG_INFO("custom", "TestLoginHook: OnLogin triggered for player: %s", player->GetName().c_str());
+        //ChatHandler(player->GetSession()).PSendSysMessage(">> TEST: Login hook triggered.");
+        //TC_LOG_INFO("custom", "TestLoginHook: OnLogin triggered for player: %s", player->GetName().c_str());
     }
 };
 
