96aa1e4120b5776b267e2d88ceec424fc65c7855
biglad <biglad@mgawow.co.uk>
2025-05-23 21:14:37 +0100

    Update cs_discord_verify.cpp

--- src/server/scripts/Custom/cs_discord_verify.cpp
+++ src/server/scripts/Custom/cs_discord_verify.cpp
@@ -1,6 +1,9 @@
 #include "ScriptMgr.h"
 #include "Chat.h"
+#include "Player.h"
+#include "WorldSession.h"
 #include <unordered_map>
+#include <ctime>
 
 std::unordered_map<uint64, std::string> g_DiscordCodes;
 
@@ -9,24 +12,23 @@ class discord_verify_commandscript : public CommandScript
 public:
     discord_verify_commandscript() : CommandScript("discord_verify_commandscript") { }
 
-    std::vector<ChatCommand> GetCommands() const override
+    ChatCommandTable GetCommands() const override
     {
-        static std::vector<ChatCommand> commands =
+        static ChatCommandTable commands =
         {
-            { "getdiscordcode", SEC_PLAYER, false, &HandleGetDiscordCode, "" },
+            { "getdiscordcode", SEC_PLAYER, false, [](ChatHandler* handler, std::string_view /*args*/) -> bool
+                {
+                    Player* player = handler->GetSession()->GetPlayer();
+                    uint64 guid = player->GetGUID();
+                    std::string code = GenerateCode(6);
+                    g_DiscordCodes[guid] = code;
+
+                    handler->PSendSysMessage("Join our Discord and DM the bot with this code: |cffffcc00%s|r", code.c_str());
+                    return true;
+                }, "" },
         };
+
         return commands;
-    }
-
-    static bool HandleGetDiscordCode(ChatHandler* handler, const char* /*args*/)
-    {
-        Player* player = handler->GetSession()->GetPlayer();
-        uint64 guid = player->GetGUID();
-        std::string code = GenerateCode(6);
-        g_DiscordCodes[guid] = code;
-
-        handler->PSendSysMessage("Join our Discord and DM the bot with this code: |cffffcc00%s|r", code.c_str());
-        return true;
     }
 
     static std::string GenerateCode(size_t length)
@@ -34,6 +36,8 @@ public:
         static const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         std::string result;
         result.reserve(length);
+
+        std::srand(std::time(nullptr)); // seed once per call
         for (size_t i = 0; i < length; ++i)
             result += charset[rand() % (sizeof(charset) - 1)];
         return result;
