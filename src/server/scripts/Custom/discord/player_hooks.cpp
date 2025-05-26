ba9903c30d969a5d1cb8862cc34ba9b464036a75
biglad <biglad@mgawow.co.uk>
2025-05-26 15:57:44 +0100

    Update player_hooks.cpp

--- src/server/scripts/Custom/discord/player_hooks.cpp
+++ src/server/scripts/Custom/discord/player_hooks.cpp
@@ -41,7 +41,7 @@ public:
         TC_LOG_INFO("player.hooks", "DiscordWebhookPlayerActivity script loaded.");
     }
 
-    void OnAchievementEarned(Player* player, AchievementEntry const* achievement) override
+    void OnAchievementEarned(Player* player, AchievementEntry const* achievement) //override NOT USED!!
     {
         if (!sConfigMgr->GetBoolDefault("Webhook.Enabled", false))
             return;
