3801cf3f6705b8a6412ddb0ead32f301380f8e1c
biglad <biglad@mgawow.co.uk>
2025-05-26 22:26:20 +0100

    Update BattlefieldWG.cpp

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -785,7 +785,8 @@ void BattlefieldWG::OnBattleEnd(bool endByTimer)
     std::string method = endByTimer ? "defended" : "captured";
     std::string winnerMessage = "⚔️ ** Wintergrasp has ended!\\n" + owner + " has " + method + " the fortress!";
     // Send to Discord
-    SendDiscordMessage(winnerMessage);
+    if (sConfigMgr->GetBoolDefault("Webhook.Enabled", true))
+        SendDiscordMessage(winnerMessage);
     //SendDiscordMessage("✅ MGAWoW webhook test message");
 }
 
