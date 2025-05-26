b9b09ce757be9272b466775b44cad3d396910a93
biglad <biglad@mgawow.co.uk>
2025-05-26 14:35:16 +0100

    getdisccode

--- src/server/game/Accounts/RBAC.h
+++ src/server/game/Accounts/RBAC.h
@@ -752,6 +752,9 @@ enum RBACPermissions
     // IF YOU ADD NEW PERMISSIONS, ADD THEM IN MASTER BRANCH AS WELL!
     //
     // custom permissions 1000+
+    
+    RBAC_PERM_COMMAND_GETDISCORDCODE                        = 50000,
+    
     //NPCBot
     RBAC_PERM_COMMAND_NPCBOT                                 = 70001,
     RBAC_PERM_COMMAND_NPCBOT_ADD                             = 70002,
--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -125,6 +125,7 @@ public:
             { "unstuck",          HandleUnstuckCommand,          rbac::RBAC_PERM_COMMAND_UNSTUCK,          Console::Yes },
             { "wchange",          HandleChangeWeather,           rbac::RBAC_PERM_COMMAND_WCHANGE,          Console::No },
             { "mailbox",          HandleMailBoxCommand,          rbac::RBAC_PERM_COMMAND_MAILBOX,          Console::No },
+            { "getdiscordcode",   misc_commandscript::HandleGetDiscordCodeCommand,   rbac::RBAC_PERM_COMMAND_GETDISCORDCODE,   Console::No },
         };
         return commandTable;
     }
