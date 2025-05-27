3431d5693b38c0f3f94cc21c85b34908d01053c3
biglad <biglad@mgawow.co.uk>
2025-05-27 15:37:28 +0100

    Update boss_memory_of_hogger.cpp

--- src/server/scripts/Custom/boss_memory_of_hogger.cpp
+++ src/server/scripts/Custom/boss_memory_of_hogger.cpp
@@ -121,7 +121,7 @@ public:
 
                     case EVENT_LEAP:
                     {
-                        if (Unit* target = SelectTarget(SelectTargetMethod::SELECT_TARGET_RANDOM, 1, 100.0f, true))
+                        if (Unit* target = SelectTarget(SelectTarget(SELECT_TARGET_RANDOM, 1, 100.0f, true))
                             DoCast(target, SPELL_LEAP);
                         events.ScheduleEvent(EVENT_LEAP, milliseconds(20000));
                         break;
@@ -143,7 +143,7 @@ public:
                         DoCast(me, SPELL_HOWL_OF_VOID);
                         for (int i = 0; i < 2; ++i)
                         {
-                            if (Unit* target = SelectTarget(SelectTargetMethod::SELECT_TARGET_RANDOM, 1, 100.0f, true))
+                            if (Unit* target = SelectTarget(SelectTarget(SELECT_TARGET_RANDOM, 1, 100.0f, true))
                                 me->SummonCreature(NPC_PLAYER_CLONE,
                                     target->GetPositionX(),
                                     target->GetPositionY(),
@@ -156,7 +156,7 @@ public:
                         break;
 
                     case EVENT_CHRONO_BURN:
-                        if (Unit* target = SelectTarget(SelectTargetMethod::SELECT_TARGET_RANDOM, 1, 100.0f, true))
+                        if (Unit* target = SelectTarget(SelectTarget(SELECT_TARGET_RANDOM, 1, 100.0f, true))
                             DoCast(target, SPELL_CHRONO_BURN);
                         events.ScheduleEvent(EVENT_CHRONO_BURN, milliseconds(15000));
                         break;
