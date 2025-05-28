eb8deb687d017319f850608733efeaecb3179450
biglad <biglad@mgawow.co.uk>
2025-05-28 16:47:57 +0100

    Update boss_memory_of_hogger.cpp

--- src/server/scripts/Custom/boss_memory_of_hogger.cpp
+++ src/server/scripts/Custom/boss_memory_of_hogger.cpp
@@ -333,6 +333,11 @@ public:
                 events.ScheduleEvent(EVENT_GNOLL_REINFORCEMENTS, milliseconds(8000));
                 events.ScheduleEvent(EVENT_LEAP, milliseconds(20000));
             }
+            
+            if (phaseTwo && HealthBelowPct(50))
+            {
+                events.ScheduleEvent(EVENT_MEMORY_OVERLOAD, milliseconds(1000));
+            }
 
             if (!phaseThree && HealthBelowPct(30))
             {
