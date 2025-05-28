c8b1f078de882140c15d89c2a0b625381144fbcf
biglad <biglad@mgawow.co.uk>
2025-05-28 21:53:20 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1006,6 +1006,24 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     sScriptMgr->OnPlayerLogin(pCurrChar, firstLogin);
 
     TC_METRIC_EVENT("player_events", "Login", pCurrChar->GetName());
+    
+    // say something as player logs in
+    //if (pCurrChar->IsAlive())
+    //{
+    //    pCurrChar->Say("MGAWoW", LANG_UNIVERSAL);  
+    //}
+    //MGAWoW Auto Invite to world channel
+    // TODO ONLY ASK IF NOT IN CHANNEL
+    
+
+
+    std::string m_name = "world";  // in-game channel name
+    data.Initialize(SMSG_CHANNEL_NOTIFY, 1 + m_name.size() + 1);
+    data << uint8(CHAT_INVITE_NOTICE);  // Inviting message
+    data << m_name.c_str();            // Channel name ("world")
+    data << uint64(pCurrChar->GetGUID());  // Player GUID for invite
+    
+    pCurrChar->GetSession()->SendPacket(&data);
 }
 
 void WorldSession::SendFeatureSystemStatus()
