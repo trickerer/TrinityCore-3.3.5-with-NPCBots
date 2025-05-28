f9a6273f4c00cc4aa35c689a6ef9777bdc524b2d
biglad <biglad@mgawow.co.uk>
2025-05-28 17:51:12 +0100

    Update boss_memory_of_hogger.cpp

--- src/server/scripts/Custom/boss_memory_of_hogger.cpp
+++ src/server/scripts/Custom/boss_memory_of_hogger.cpp
@@ -365,14 +365,14 @@ public:
                 switch (eventId)
                 {
                     case EVENT_CLEAVE:
-                    {
+                    
                         DoCastVictim(SPELL_CLEAVE);
                         events.ScheduleEvent(EVENT_CLEAVE, milliseconds(6000));
-                        break;
-                    }
+                    break;
+                    
 
                     case EVENT_LEAP:
-                    {
+                    
                         me->Yell("LEAP!", LANG_UNIVERSAL);
 
                         Unit* furthestTarget = nullptr;
@@ -416,11 +416,11 @@ public:
                         }
 
                         events.ScheduleEvent(EVENT_LEAP, milliseconds(20000));
-                        break;
-                    }
+                    break;
+                    
 
                     case EVENT_GNOLL_REINFORCEMENTS:
-                    {
+                    
                         me->Yell("Come Forth My Minions, Assist Me!", LANG_UNIVERSAL);
                         for (int i = 0; i < 3; ++i)
                         {
@@ -448,10 +448,10 @@ public:
                             events.ScheduleEvent(EVENT_GNOLL_REINFORCEMENTS, milliseconds(20000));
                         else
                             events.ScheduleEvent(EVENT_GNOLL_REINFORCEMENTS, milliseconds(30000));
-                        break;
-                    }
+                    break;
+                    
                     case EVENT_HOWL_OF_VOID:
-                    {
+                    
                         DoCast(me, SPELL_HOWL_OF_VOID);
                         //me->Yell("VOID!!!", LANG_UNIVERSAL);
                         for (int i = 0; i < 2; ++i)
@@ -471,33 +471,33 @@ public:
                             }
                         }
                         events.ScheduleEvent(EVENT_HOWL_OF_VOID, milliseconds(45000));
-                        break;
-                    }
+                    break;
+                    
                     case EVENT_CHRONO_BURN:
-                    {
+                    
                         //me->Yell("CHRONO BURN!!", LANG_UNIVERSAL);
                         if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 200, true))
                             DoCast(target, SPELL_CHRONO_BURN);
                         events.ScheduleEvent(EVENT_CHRONO_BURN, milliseconds(15000));
-                        break;
-                    }
+                    break;
+                    
                     case EVENT_UNSTABLE_RIFT:
-                    {
+                    
                         // Optional hazard here
                         DoCast(me, SPELL_UNSTABLE_RIFT);
                         //me->Yell("SUNSTABLE RIFT", LANG_UNIVERSAL);
                         events.ScheduleEvent(EVENT_UNSTABLE_RIFT, milliseconds(15000));
-                        break;
-                    }
+                    break;
+   
                     case EVENT_ECHO_SLAM:
-                    {
+                    
                         //me->Yell("SLAM", LANG_UNIVERSAL);
                         DoCast(me, SPELL_ECHO_SLAM);
                         events.ScheduleEvent(EVENT_ECHO_SLAM, milliseconds(15000));
-                        break;
-                    }
+                    break;
+                    
                     case EVENT_MEMORY_OVERLOAD:
-                    {
+                    
                         //me->Yell("MEMORY OVER LOAD!", LANG_UNIVERSAL);
                         for (int i = 0; i < 4; ++i)
                         {
@@ -516,8 +516,8 @@ public:
                             }
                         }
                         events.ScheduleEvent(EVENT_MEMORY_OVERLOAD, milliseconds(50000));
-                        break;
-                    }
+                    break;
+                    
                     default:
                         break;
                 }
