cae913ec08cf21a1104f2b21ed76005013f656e0
biglad <biglad@mgawow.co.uk>
2025-05-29 23:10:29 +0100

    Update Channel.cpp

--- src/server/game/Chat/Channels/Channel.cpp
+++ src/server/game/Chat/Channels/Channel.cpp
@@ -32,6 +32,7 @@
 #include "SocialMgr.h"
 #include "StringConvert.h"
 #include "World.h"
+#include "Config.h"
 #include "../../scripts/Custom/discord/DiscordWebhookMgr.h"
 
 Channel::Channel(uint32 channelId, uint32 team /*= 0*/, AreaTableEntry const* zoneEntry /*= nullptr*/) :
