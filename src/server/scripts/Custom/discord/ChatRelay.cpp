a01351aca2c5ab05815f0312bf7c6053926486af
biglad <biglad@mgawow.co.uk>
2025-05-29 19:44:13 +0100

    123

---
+++ src/server/scripts/Custom/discord/ChatRelay.cpp
@@ -0,0 +1,36 @@
+#include "Player.h"
+#include "PlayerScript.h"
+#include "Chat.h"
+#include "Log.h"
+#include "DiscordWebhookMgr.h" // Your header where SendDiscordMessage is declared
+
+class ChatRelayScript : public PlayerScript
+{
+public:
+    ChatRelayScript() : PlayerScript("ChatRelayScript") {}
+
+    void OnChat(Player* player, uint32 type, uint32 lang, std::string& msg, Player* receiver) override
+    {
+        // We want only world channel messages
+        // CHAT_MSG_CHANNEL = 7 for channel chat; world channel typically named "world"
+        if (type == CHAT_MSG_CHANNEL)
+        {
+            if (channelName == "world")
+            {
+                if (msg.find("[Discord]:") == std::string::npos)
+                {
+                    // Compose the message to send to Discord webhook
+                    std::string discordMsg = "[World] [" + player->GetName() + "]: " + msg;
+
+                    // Send to Discord
+                    SendDiscordMessage(discordMsg);
+                }
+            }
+        }
+    }
+};
+
+void AddSC_chat_relay_script()
+{
+    new ChatRelayScript();
+}
