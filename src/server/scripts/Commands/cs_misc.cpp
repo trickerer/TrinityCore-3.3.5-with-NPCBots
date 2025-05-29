8d54dffe7f3736c087f54e36a093288f733aba00
biglad <biglad@mgawow.co.uk>
2025-05-29 15:00:39 +0100

    Update cs_misc.cpp

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -164,7 +164,7 @@ public:
             return false;
         }
 
-        channel->SayBot("Discord", args, LANG_UNIVERSAL);
+        channel->Say("Discord", args, LANG_UNIVERSAL);
 
         handler->SendSysMessage("Message sent as Discord bot.");
         return true;
