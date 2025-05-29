16f27475d19d14a92155a6f57c6a6f4aaae310a4
biglad <biglad@mgawow.co.uk>
2025-05-29 17:31:21 +0100

    Update Channel.cpp

--- src/server/game/Chat/Channels/Channel.cpp
+++ src/server/game/Chat/Channels/Channel.cpp
@@ -691,7 +691,7 @@ void Channel::SayAsFake(Player* sender, std::string const& senderName, std::stri
 
         data << uint32(0);                          // Account ID (fake)
         data << senderName;                         // Sender name
-        data << GetChannelName();                               // Channel name (use actual channel name)
+        data << GetName();                               // Channel name (use actual channel name)
         data << uint64(ObjectGuid::Empty);         // Receiver GUID (empty for channels)
         data << message;                            // Message text
         data << uint8(0);                           // Chat tag
