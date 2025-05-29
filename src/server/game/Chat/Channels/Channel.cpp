ca1a1b10040cd3c50abbd6f496fc8ee3398bb58c
biglad <biglad@mgawow.co.uk>
2025-05-29 17:44:35 +0100

    Update Channel.cpp

--- src/server/game/Chat/Channels/Channel.cpp
+++ src/server/game/Chat/Channels/Channel.cpp
@@ -691,7 +691,7 @@ void Channel::SayAsFake(Player* sender, std::string const& senderName, std::stri
         }
         else
         {
-            data << uint64(ObjectGuid(HighGuid::Player, 0, 3125)); // dummy valid player GUID
+            data << uint64(ObjectGuid(HighGuid::Player, 0, static_cast<ObjectGuid::LowType>(3125))); // dummy valid player GUID
             data << uint32(1); // fake account ID
         }
 
