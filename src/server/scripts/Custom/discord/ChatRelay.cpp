589bd316cf5a617ba3f66d56509d04270c1f2abe
biglad <biglad@mgawow.co.uk>
2025-05-29 19:58:14 +0100

    Update ChatRelay.cpp

--- src/server/scripts/Custom/discord/ChatRelay.cpp
+++ src/server/scripts/Custom/discord/ChatRelay.cpp
@@ -12,18 +12,14 @@ public:
     {
 
         // Get the "world" channel (realm ID 0)
+        ChannelMgr* cMgr = ChannelMgr::forTeam(TEAM_NEUTRAL);
         Channel* channel = cMgr->GetChannel(0, "world", player, false, nullptr);
         if (!channel)
             return;
 
-        // Check if player is a member of the "world" channel
-        if (!channel->HasMember(player->GetGUID()))
-            return;
 
-        // Only proceed if message type is say / yell / whatever you want, or just log all
-        // For example, let's just print all messages from the world channel:
         std::string playerName = player->GetName();
-        TC_LOG_INFO("chatrelay", "[WORLD CHANNEL] <%s>: %s", playerName.c_str(), msg.c_str());
+        TC_LOG_INFO("chatrelay", "[WORLD CHANNEL] {}: {}", playerName, msg);
 
         // Here you could add your Discord webhook or other integration
     }
