5cbb6903f88a940c387a51c26322599fa89bdd29
biglad <biglad@mgawow.co.uk>
2025-05-30 22:34:08 +0100

    321

--- src/server/scripts/Custom/npc_proff_master.cpp
+++ src/server/scripts/Custom/npc_proff_master.cpp
@@ -153,21 +153,21 @@ public:
 
             WorldSession* session = player->GetSession();
             // CHEDCK IF FREE SKILL SLOT
-            AddGossipItemFor(player, GOSSIP_ICON_DOT, "Main Professions Cost 3 Mini Tokens", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
-            AddGossipItemFor(player, GOSSIP_ICON_DOT, "Secondary Professions Cost 2 Mini Tokens", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "Main Professions Cost 3 Mini Tokens", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "Secondary Professions Cost 2 Mini Tokens", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
             AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "--", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
-            AddGossipItemFor(player, GOSSIP_ICON_DOT, "Main Professions", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "Main Professions", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
             if (freeProfs < 1)
             {
-                AddGossipItemFor(player, GOSSIP_ICON_DOT, GOSSIP_HELLO_NOFREE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, GOSSIP_HELLO_NOFREE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
             }
             else
             {
                 // AD TOO MAIN PROFFS
                 if (freeProfs == 2)
-                    AddGossipItemFor(player, GOSSIP_ICON_DOT, "You can learn " + proffcount + " main professions", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+                    AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "You can learn " + proffcount + " main professions", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
                 if (freeProfs == 1)
-                    AddGossipItemFor(player, GOSSIP_ICON_DOT, "You can learn " + proffcount + " main profession", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+                    AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "You can learn " + proffcount + " main profession", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
 
                 
                 if (!player->HasSkill(BlackSmithSkill))
@@ -195,24 +195,24 @@ public:
 
 
             }
-            AddGossipItemFor(player, GOSSIP_ICON_DOT, "-", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
-            AddGossipItemFor(player, GOSSIP_ICON_DOT, "Secondary Professions", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "-", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "Secondary Professions", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9999);
 
             // 2nd PROFFS
             if (!player->HasSkill(FirstAidSkill))
                 AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Learn First Aid", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1000);  //1stAid
             else
-                AddGossipItemFor(player, GOSSIP_ICON_DOT, "<Unlearn First Aid>", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3000);  //1stAid
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "<Unlearn First Aid>", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3000);  //1stAid
 
             if (!player->HasSkill(CookingSkill))
                 AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Learn Cooking", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1001);  //cooking
             else
-                AddGossipItemFor(player, GOSSIP_ICON_DOT, "<Unlearn Cooking>", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3001);  //cooking
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "<Unlearn Cooking>", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3001);  //cooking
 
             if (!player->HasSkill(FishingSkill))
                 AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Learn Fishing", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1002);  //fishing
             else
-                AddGossipItemFor(player, GOSSIP_ICON_DOT, "<Unlearn Fishing>", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3002);  //fishing
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_2, "<Unlearn Fishing>", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3002);  //fishing
 
             AddGossipItemFor(player, GOSSIP_ICON_TALK, GOSSIP_HELLO_BYE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2000);
 
