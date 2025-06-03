0b462cd97c2c4448e40f9d570afda9fd62aee02b
biglad <biglad@mgawow.co.uk>
2025-06-03 12:38:02 +0100

    Update npc_token_exchange.cpp

--- src/server/scripts/Custom/npc_token_exchange.cpp
+++ src/server/scripts/Custom/npc_token_exchange.cpp
@@ -466,7 +466,10 @@ public:
                         {
                             Item* item = player->StoreNewItem(dest, itemId, 1, true);
                             player->SendNewItem(item, 1, true, false);
-                            player->ModifyMoney(-4500000000);
+                            player->ModifyMoney(-1000000000);
+                            player->ModifyMoney(-1000000000);
+                            player->ModifyMoney(-1000000000);
+                            player->ModifyMoney(-1500000000);
                             me->Say(tokentext8, LANG_UNIVERSAL); // Success
                         }
                         else
