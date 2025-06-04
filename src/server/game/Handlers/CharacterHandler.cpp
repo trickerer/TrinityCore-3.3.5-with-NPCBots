6039ef9a291d0409344d3d5633371c8e595e3ee3
biglad <biglad@mgawow.co.uk>
2025-06-04 23:44:51 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1024,9 +1024,9 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     //MGAWoW Auto Invite to world channel
     // TODO ONLY ASK IF NOT IN CHANNEL
     SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(80875);
-    if (!spellInfo)
+    if (spellInfo && !player->CanCast(spellInfo))
     {
-        pCurrChar->Yell("I Don't hvae the MGAWoW Client, MGAWoW custom content will not work for me! - Download out client here https://mgawow.online/downlaod/ - This is optional", LANG_UNIVERSAL);  
+        pCurrChar->Yell("I don't have the MGAWoW Client, MGAWoW custom content will not work for me! - Download our client here https://mgawow.online/download/ - This is optional", LANG_UNIVERSAL);
     }
 
     std::string m_name = "world";  // in-game channel name
