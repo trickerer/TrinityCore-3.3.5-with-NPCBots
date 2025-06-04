20e3b46f806839ff51b8413c93031d4c4572bb62
biglad <biglad@mgawow.co.uk>
2025-06-04 22:57:54 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1023,7 +1023,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     //MGAWoW Auto Invite to world channel
     // TODO ONLY ASK IF NOT IN CHANNEL
     SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(80875);
-    if (player->CanCast(spellInfo))
+    if (pCurrChar->CanCast(spellInfo))
     {
         pCurrChar->Say("You Have the MGAWoW Client!", LANG_UNIVERSAL);  
     }
