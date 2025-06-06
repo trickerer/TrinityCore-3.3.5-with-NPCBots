4f88c969d5620995fa10eded8fc39b61a086e919
biglad <biglad@mgawow.co.uk>
2025-06-06 19:21:01 +0100

    Update mga_boss_one.cpp

--- src/server/scripts/Custom/mga_boss_one.cpp
+++ src/server/scripts/Custom/mga_boss_one.cpp
@@ -382,7 +382,7 @@ public:
 
     struct mga_boss_oneAI : public BossAI
     {
-        mga_boss_oneAI(Creature* creature) : BossAI(creature)
+        mga_boss_oneAI(Creature* creature) : ScriptedAI(creature)
         {
         }
 
@@ -507,45 +507,44 @@ public:
 			{
 				if (me->IsValidAttackTarget(who) && !HasStarted)
 				{
-				WorldDatabase.PExecute(_QUERY2_);
-				me->PlayDirectSound(15771) ; // UR_FemaleYogg_Prefight01.wav
-				HasStarted = true;
-				EnterCombat(who);
-				DoCast(who, SPELL_ROOT); // root
-				me->GetThreatManager().AddThreat(who, 10.0f);
-				DoCast(me, SPELL_SHIELD);
-				//me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE + UNIT_FLAG_NON_ATTACKABLE);
-				//me->GetMotionMaster()->MovePoint(1, me->GetPositionX(),me->GetPositionY(),me->GetPositionZ()+15);
-				MGAImmune = true;
-				if (me->GetEntry() == NPC_BOSS_MEDMODE)
-				{
-					me->SummonCreature(NPC_GUARD_MEDMODE, who->GetPositionX()+5, who->GetPositionY()+5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-					me->SummonCreature(NPC_GUARD_MEDMODE, who->GetPositionX()-5, who->GetPositionY()-5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-				}
-				else if (me->GetEntry() == NPC_BOSS_HARDMODE)
-				{
-					me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()+5, who->GetPositionY()+5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-					me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()-5, who->GetPositionY()-5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-					me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()+10, who->GetPositionY()+10, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-					me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()-10, who->GetPositionY()-10, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-				}
-				else
-				{
-					me->SummonCreature(NPC_GUARD, who->GetPositionX()+5, who->GetPositionY()+5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-					me->SummonCreature(NPC_GUARD, who->GetPositionX()-5, who->GetPositionY()-5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
-				}
-				SlimePoolCD = 18000;
-				me->SummonCreature(NPC_SLIME, who->GetPositionX(), who->GetPositionY(), who->GetPositionZ(), 0.f, TEMPSUMMON_TIMED_DESPAWN, milliseconds(30000));
-				me->Yell("Minions Attack The Intruders", LANG_UNIVERSAL, NULL);
-				DoStartNoMovement(who);
+                    WorldDatabase.PExecute(_QUERY2_);
+                    me->PlayDirectSound(15771) ; // UR_FemaleYogg_Prefight01.wav
+                    HasStarted = true;
+                    EnterCombat(who);
+                    DoCast(who, SPELL_ROOT); // root
+                    me->GetThreatManager().AddThreat(who, 10.0f);
+                    DoCast(me, SPELL_SHIELD);
+                    //me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE + UNIT_FLAG_NON_ATTACKABLE);
+                    //me->GetMotionMaster()->MovePoint(1, me->GetPositionX(),me->GetPositionY(),me->GetPositionZ()+15);
+                    MGAImmune = true;
+                    if (me->GetEntry() == NPC_BOSS_MEDMODE)
+                    {
+                        me->SummonCreature(NPC_GUARD_MEDMODE, who->GetPositionX()+5, who->GetPositionY()+5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                        me->SummonCreature(NPC_GUARD_MEDMODE, who->GetPositionX()-5, who->GetPositionY()-5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                    }
+                    else if (me->GetEntry() == NPC_BOSS_HARDMODE)
+                    {
+                        me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()+5, who->GetPositionY()+5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                        me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()-5, who->GetPositionY()-5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                        me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()+10, who->GetPositionY()+10, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                        me->SummonCreature(NPC_GUARD_HARDMODE, who->GetPositionX()-10, who->GetPositionY()-10, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                    }
+                    else
+                    {
+                        me->SummonCreature(NPC_GUARD, who->GetPositionX()+5, who->GetPositionY()+5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                        me->SummonCreature(NPC_GUARD, who->GetPositionX()-5, who->GetPositionY()-5, who->GetPositionZ(), 0.f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, milliseconds(2000));
+                    }
+                    SlimePoolCD = 18000;
+                    me->SummonCreature(NPC_SLIME, who->GetPositionX(), who->GetPositionY(), who->GetPositionZ(), 0.f, TEMPSUMMON_TIMED_DESPAWN, milliseconds(30000));
+                    me->Yell("Minions Attack The Intruders", LANG_UNIVERSAL, NULL);
+                    DoStartNoMovement(who);
 				}
 			}
-            
 		
 		}
         void JustEngagedWith(Unit* who) override
         {
-            
+            BossAI::JustEngagedWith(who);
         }
 		
 		void EnterCombat(Unit* Who)
