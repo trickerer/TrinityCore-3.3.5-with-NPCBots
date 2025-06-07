b18ce143b6c13449dbbecb73ee6ca3612970c22a
biglad <biglad@mgawow.co.uk>
2025-06-07 19:55:39 +0100

    123

--- src/server/scripts/Custom/boss_memory_of_hogger.cpp
+++ src/server/scripts/Custom/boss_memory_of_hogger.cpp
@@ -270,7 +270,6 @@ public:
             me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_SLOW_ATTACK, true);
             me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_DECREASE_SPEED, true);
             me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_ROOT, true);
-            me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_BLIND, true);
             me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_BANISH, true); 
             me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_SAPPED , true);
         }
