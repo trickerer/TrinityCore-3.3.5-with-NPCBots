6b77ff5da4e636d1ea9fb0619604f89be30caa6f
biglad <biglad@mgawow.co.uk>
2025-06-08 18:25:35 +0100

    Update custom_flightpath_item.cpp

--- src/server/scripts/Custom/custom_flightpath_item.cpp
+++ src/server/scripts/Custom/custom_flightpath_item.cpp
@@ -66,7 +66,7 @@ public:
             player->DestroyItemCount(461146, 1, true);
             player->SetTaxiCheater(true);
 
-            std::string query = Trinity::StringFormat("UPDATE characters SET taximask = '%llu' WHERE guid = '%u'", taxiMask, player->GetGUID().GetCounter());
+            std::string query = Trinity::StringFormat("UPDATE characters SET taximask = '{}' WHERE guid = '{}'", taxiMask, player->GetGUID().GetCounter());
             CharacterDatabase.Execute(query.c_str());
 
             player->GetSession()->SendAreaTriggerMessage("You have learned %u flight paths.", count);
