11696ab12d0e93491abc4eba44171e99fae965ef
biglad <biglad@mgawow.co.uk>
2025-06-08 17:11:51 +0100

    Update custom_flightpath_item.cpp

--- src/server/scripts/Custom/custom_flightpath_item.cpp
+++ src/server/scripts/Custom/custom_flightpath_item.cpp
@@ -36,7 +36,12 @@ public:
         if (count > 0)
         {
             player->DestroyItemCount(461146, 1, true);
-            player->GetSession()->SendTaxiStatus();
+            WorldPacket data(SMSG_NEW_TAXI_PATH, 8 + 72);
+            data << player->GetPackGUID();
+            for (uint8 i = 0; i < TaxiMaskSize; ++i)
+                data << uint32(player->m_taxi.GetTaximask()[i]);
+            player->SendDirectMessage(&data);
+            
             player->GetSession()->SendAreaTriggerMessage("You have learned %u flight paths.", count);
             ChatHandler(player->GetSession()).PSendSysMessage("Learned %u flight paths.", count);
             player->SaveToDB();
