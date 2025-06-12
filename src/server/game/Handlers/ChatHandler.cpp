2d4f3b51a008ff5945cef0b17c1c71321359ca8b
biglad <biglad@mgawow.co.uk>
2025-06-12 21:09:02 +0100

    Update ChatHandler.cpp

--- src/server/game/Handlers/ChatHandler.cpp
+++ src/server/game/Handlers/ChatHandler.cpp
@@ -231,7 +231,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
             return;
     }
     
-    
+    recvData >> type >> lang >> channel >> to >> msg;
     if (type == CHAT_MSG_WHISPER && lang == LANG_ADDON)
     {
         // Parse prefix and message from the addon msg string
