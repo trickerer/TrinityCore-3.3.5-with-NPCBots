cd0a6ab6129d5ec27d5da7f5b022143b24ee4fb3
biglad <biglad@mgawow.co.uk>
2025-06-12 18:27:35 +0100

    Update ChatHandler.cpp

--- src/server/game/Handlers/ChatHandler.cpp
+++ src/server/game/Handlers/ChatHandler.cpp
@@ -228,7 +228,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
             return;
     }
     
-    /*
+    
     std::string prefix;
     recvData >> prefix >> msg;
     if ((type == CHAT_MSG_WHISPER) && (lang == LANG_ADDON))
@@ -251,10 +251,8 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
             //    _player->GetGUID().GetCounter()
             //);
             _player->GetSession()->SendNotification("You are using the HD client!");
-
-            return; // prevent further handling
         }
-    }*/
+    }
 
     // no chat commands in AFK/DND autoreply, and it can be empty
     if (!(type == CHAT_MSG_AFK || type == CHAT_MSG_DND))
