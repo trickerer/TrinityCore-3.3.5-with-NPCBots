b2293a4947c92ddba1d96c276126b51fe6fdeae6
biglad <biglad@mgawow.co.uk>
2025-06-12 19:12:23 +0100

    123

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -769,6 +769,7 @@ class TC_GAME_API World
         void TriggerGuidAlert();
         bool IsGuidWarning() { return _guidWarn; }
         bool IsGuidAlert() { return _guidAlert; }
+        void HandleAddonMessage(WorldPacket& recvData);
 
     protected:
         void _UpdateGameTime();
