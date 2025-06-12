406883bb348ed417453f616afcef574d937263ab
biglad <biglad@mgawow.co.uk>
2025-06-12 17:27:39 +0100

    Update ChatHandler.cpp

--- src/server/game/Handlers/ChatHandler.cpp
+++ src/server/game/Handlers/ChatHandler.cpp
@@ -60,9 +60,6 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
 
     recvData >> type;
     recvData >> lang;
-    //MGAHD ADDON
-    std::string prefix;
-    recvData >> prefix >> msg;
 
     if (type >= MAX_CHAT_MSG_TYPE)
     {
@@ -231,6 +228,9 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
             return;
     }
     
+    
+    std::string prefix;
+    recvData >> prefix >> msg;
     if ((type == CHAT_MSG_WHISPER) && (lang == LANG_ADDON))
     {
         // Existing warden Lua check
