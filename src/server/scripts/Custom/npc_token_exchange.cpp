c9caf6cfaea7cbac0bb283a718ba6caee9db6256
biglad <biglad@mgawow.co.uk>
2025-06-14 16:51:37 +0100

    Update npc_token_exchange.cpp

--- src/server/scripts/Custom/npc_token_exchange.cpp
+++ src/server/scripts/Custom/npc_token_exchange.cpp
@@ -9,8 +9,6 @@
 #include "WorldSession.h"
 #include "ObjectMgr.h"
 #include "Chat.h"
-#include "World.h"
-#include "SpellAuraDefines.h"
 #include <sstream>
 #include <string>
 
@@ -507,21 +505,15 @@ public:
                 break;
                 case 1019:
                 CloseGossipMenuFor(player);
-                if (player->HasItemCount(989891, 1)) 
-                {
+                if (player->HasItemCount(989891, 1)) {
                     uint8 level = player->GetLevel();
                     uint32 xpForNextLevel = sObjectMgr->GetXPForLevel(level);
                     float xpPercent = 0.20f;
-                    float rate = sWorld->getRate(RATE_XP_KILL); 
-                    float restedBonus = player->GetRestBonus(); 
-                    float heirloomBonus = player->GetTotalAuraModifier(SPELL_AURA_MOD_XP_QUEST_PCT); 
-                    
-                    float totalMultiplier = (rate * restedBonus) * (1.0f + heirloomBonus);
-                    uint32 xpToGive = static_cast<uint32>(xpForNextLevel * xpPercent * totalMultiplier);
+                    uint32 xpToGive = static_cast<uint32>(xpForNextLevel * xpPercent);
 
                     player->GiveXP(xpToGive, nullptr);
                     player->DestroyItemCount(989891, 1, true);
-                    player->GetSession()->SendAreaTriggerMessage("You gained %u XP!", xpToGive);
+                    ChatHandler(player->GetSession()).PSendSysMessage("You gained %u XP!", xpToGive);
                 }
                 break;
             }
