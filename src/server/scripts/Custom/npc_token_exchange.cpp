475d1968fc8a955d378c83276405b1304c4a48cf
biglad <biglad@mgawow.co.uk>
2025-06-14 17:24:44 +0100

    Update npc_token_exchange.cpp

--- src/server/scripts/Custom/npc_token_exchange.cpp
+++ src/server/scripts/Custom/npc_token_exchange.cpp
@@ -519,7 +519,7 @@ public:
                 }
                 else
                 {
-                    me->Yell(tokentext2, LANG_UNIVERSAL);
+                    me->Yell("You are missing a MGA XP Token!", LANG_UNIVERSAL);
                 }
                 break;
             }
