7ad56ca7949ba04687f7e5455420e91554922b79
biglad <biglad@mgawow.co.uk>
2025-06-15 15:19:14 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1031,9 +1031,9 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     //char buffer[64];
     //sprintf(buffer, "GUID: %u", guid);
     //pCurrChar->Say(buffer, LANG_UNIVERSAL);
-    QueryResult result = CharacterDatabase.PQuery("SELECT in_world_channel FROM world_channel_flags WHERE guid = %u", guid);
+    QueryResult result = CharacterDatabase.PQuery("SELECT in_world_channel FROM world_channel_flags WHERE guid = {}", guid);
     
-    if (!result || !result->Fetch()[0].GetBool())
+    if (!result || result->GetRowCount() == 0 || !result->Fetch()[0].GetBool())
     {
         std::string m_name = "world";  // in-game channel name
         data.Initialize(SMSG_CHANNEL_NOTIFY, 1 + m_name.size() + 1);
