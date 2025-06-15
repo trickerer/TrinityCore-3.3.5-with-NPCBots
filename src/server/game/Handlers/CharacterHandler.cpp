16a7c3d6e7e2754d871cf1360bd6d6bcaf00ddf9
biglad <biglad@mgawow.co.uk>
2025-06-15 15:02:01 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1026,7 +1026,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     //}
     //MGAWoW Auto Invite to world channel
     // TODO ONLY ASK IF NOT IN CHANNEL
-    QueryResult result = CharacterDatabase.PQuery("SELECT in_world_channel FROM world_channel_flags WHERE guid = {}", pCurrChar->GetGUID().GetCounter());
+    QueryResult result = CharacterDatabase.PQuery("SELECT in_world_channel FROM world_channel_flags WHERE guid = {}", uint64(pCurrChar->GetGUID()));
     if (!result || !result->Fetch()[0].GetBool())
     {
         std::string m_name = "world";  // in-game channel name
