211dcbab6b242020587dd85f3f8c6ff9592d0a02
biglad <biglad@mgawow.co.uk>
2025-06-17 01:05:15 +0100

    Update Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22689,7 +22689,7 @@ void Player::InitPrimaryProfessions()
 {
     SetFreePrimaryProfessions(sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL));
     if (HasItemCount(461145, 1))
-        SetFreePrimaryProfessions = SetFreePrimaryProfessions+2;
+        SetFreePrimaryProfessions()+2;
 }
 
 bool Player::ModifyMoney(int32 amount, bool sendError /*= true*/)
