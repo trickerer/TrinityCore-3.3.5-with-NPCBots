0eff8a1f9c1c508afcc5b9c7e47085cd4377cfbd
biglad <biglad@mgawow.co.uk>
2025-06-17 02:44:32 +0100

    Update Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -3626,7 +3626,7 @@ void Player::RemoveSpell(uint32 spell_id, bool disabled, bool learn_low_rank)
     if (spellInfo && spellInfo->IsPrimaryProfessionFirstRank())
     {
         uint32 freeProfs = GetFreePrimaryProfessionPoints()+1;
-        if (freeProfs <= sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL2))
+        if (freeProfs < sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL2))
             SetFreePrimaryProfessions(freeProfs);
     }
 
