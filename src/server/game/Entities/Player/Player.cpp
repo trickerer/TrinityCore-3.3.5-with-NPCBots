71c5035561c2dc238cc801f464c79d2777f4bb02
biglad <biglad@mgawow.co.uk>
2025-06-17 02:27:33 +0100

    Update Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -3625,16 +3625,12 @@ void Player::RemoveSpell(uint32 spell_id, bool disabled, bool learn_low_rank)
     SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(spell_id);
     if (spellInfo && spellInfo->IsPrimaryProfessionFirstRank())
     {
-        uint32 maxProfs = sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL);
-        if (HasItemCount(461145, 1))
-            maxProfs = sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL2);
-
-        uint32 usedProfs = GetPrimaryProfessionCount();
-
-        if (usedProfs < maxProfs)
-            SetFreePrimaryProfessions(maxProfs - (usedProfs + 1)); // -1 for the one we're about to learn
-        else
-            SetFreePrimaryProfessions(0);
+        uint32 maxProfs = sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL2);
+        
+        uint32 freeProfs = GetFreePrimaryProfessionPoints() + 1;
+     
+        if (freeProfs <= maxProfs)
+            SetFreePrimaryProfessions(freeProfs);
     }
 
     // remove dependent skill
