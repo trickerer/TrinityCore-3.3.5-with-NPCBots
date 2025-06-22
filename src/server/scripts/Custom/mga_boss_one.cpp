01c56f20559889982aafd4b1fbbdd198aad07234
biglad <biglad@mgawow.co.uk>
2025-06-22 15:28:52 +0100

    Update mga_boss_one.cpp

--- src/server/scripts/Custom/mga_boss_one.cpp
+++ src/server/scripts/Custom/mga_boss_one.cpp
@@ -610,11 +610,11 @@ public:
                 if (Channel* channel = channelMgr->GetChannel(0, "world", player, false, nullptr))
                 {
                     if (me->GetEntry() == NPC_BOSS_HARDMODE)
-                        channel->SayAsFake(player->GetGUID(), "MGA Mega Boss has been defeated in 25 Man Mode!", LANG_UNIVERSAL);
+                        channel->SayAsFake(Player->GetGUID(), "MGA Mega Boss has been defeated in 25 Man Mode!", LANG_UNIVERSAL);
                     else if (me->GetEntry() == NPC_BOSS_MEDMODE)
-                        channel->SayAsFake(player->GetGUID(), "MGA Mega Boss has been defeated in 10 Man Mode!", LANG_UNIVERSAL);
+                        channel->SayAsFake(Player->GetGUID(), "MGA Mega Boss has been defeated in 10 Man Mode!", LANG_UNIVERSAL);
                     else
-                        channel->SayAsFake(player->GetGUID(), "MGA Mega Boss has been defeated in 5 Man Mode!", LANG_UNIVERSAL);
+                        channel->SayAsFake(Player->GetGUID(), "MGA Mega Boss has been defeated in 5 Man Mode!", LANG_UNIVERSAL);
                 }
             }
         }
