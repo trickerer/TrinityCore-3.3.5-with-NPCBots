929949bfcc3f9d19a78bfc21f0389de01f85ec0f
biglad <biglad@mgawow.co.uk>
2025-06-22 15:39:41 +0100

    Update mga_boss_one.cpp

--- src/server/scripts/Custom/mga_boss_one.cpp
+++ src/server/scripts/Custom/mga_boss_one.cpp
@@ -607,8 +607,8 @@ public:
             
             if (ChannelMgr* channelMgr = ChannelMgr::forTeam(TEAM_NEUTRAL))
             {
-                if (Channel* channel = channelMgr->GetChannel(0, "world", player, false, nullptr))
-                {
+                //if (Channel* channel = channelMgr->GetChannel(0, "world", player, false, nullptr))
+                //{
                     std::string message;
 
                     switch (me->GetEntry())
@@ -625,7 +625,7 @@ public:
                     }
 
                     channel->SayAsFake(player, me->GetName(), message, LANG_UNIVERSAL);
-                }
+                //}
             }
         }
         
