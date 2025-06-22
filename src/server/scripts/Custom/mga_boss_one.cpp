4fb6029c5c57a297ac289794f45ae3a21a2a2ad1
biglad <biglad@mgawow.co.uk>
2025-06-22 16:06:18 +0100

    Update mga_boss_one.cpp

--- src/server/scripts/Custom/mga_boss_one.cpp
+++ src/server/scripts/Custom/mga_boss_one.cpp
@@ -747,12 +747,12 @@ public:
                     me->Yell("YOU COWARDS... RUN THEN!", LANG_UNIVERSAL);
                     me->DespawnOrUnsummon();
                     // Channel message
-                    Player* player = killer ? killer->ToPlayer() : nullptr;
+                    Unit* target = SelectTarget(SelectTargetMethod::Random, 1, 2000, true);
                     ChannelMgr* channelMgr = ChannelMgr::forTeam(TEAM_NEUTRAL);
 
-                    if (player && channelMgr)
+                    if (target && channelMgr)
                     {
-                        if (Channel* channel = channelMgr->GetChannel(0, "world", player, false, nullptr))
+                        if (Channel* channel = channelMgr->GetChannel(0, "world", target, false, nullptr))
                         {
                             std::string message;
 
@@ -769,7 +769,7 @@ public:
                                     break;
                             }
 
-                            channel->SayAsFake(player, me->GetName(), message, LANG_UNIVERSAL);
+                            channel->SayAsFake(target, me->GetName(), message, LANG_UNIVERSAL);
                         }
                     }
                     //EnterEvadeMode();
