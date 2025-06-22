d0e9bfc351e8c80a58ba6ad7f4e80fcf0c5f02a6
biglad <biglad@mgawow.co.uk>
2025-06-22 15:44:49 +0100

    Update mga_boss_one.cpp

--- src/server/scripts/Custom/mga_boss_one.cpp
+++ src/server/scripts/Custom/mga_boss_one.cpp
@@ -572,7 +572,7 @@ public:
 
             // Channel message
             Player* player = killer ? killer->ToPlayer() : nullptr;
-            if (player && ChannelMgr* channelMgr = ChannelMgr::forTeam(TEAM_NEUTRAL))
+            if (player && ChannelMgr* ChannelMgr = ChannelMgr::forTeam(TEAM_NEUTRAL))
             {
                 if (Channel* channel = ChannelMgr->GetChannel(0, "world", player, false, nullptr))
                 {
