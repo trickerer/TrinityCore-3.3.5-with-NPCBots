c96611598b04e38d7877c37e8277ba7cea16a683
biglad <biglad@mgawow.co.uk>
2025-06-23 18:29:44 +0100

    Update CharacterHandler.cpp

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1045,11 +1045,11 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     }
     else
     {
-        const std::string name = pCurrChar->GetName();
-        const uint8 level = pCurrChar->GetLevel();
-        const std::string gmTag = pCurrChar->GetSession()->GetSecurity() > SEC_PLAYER 
+        std::string name = pCurrChar->GetName();
+        uint8 level = pCurrChar->GetLevel();
+        std::string gmTag = pCurrChar->GetSession()->GetSecurity() > SEC_PLAYER 
                           ? (pCurrChar->GetSession()->GetSecurity() > 3 ? "🧪 " : "⚙️ ")
-        const std::string status2 = "🟢 Join World Channel";
+        std::string status2 = "🟢 Join World Channel";
 
         std::ostringstream messageStream2;
         messageStream2 << gmTag << status2 << " `" << name << "` (Level " << static_cast<int>(level) << ")";
