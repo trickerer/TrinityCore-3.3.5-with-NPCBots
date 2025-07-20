7fd9f5cfe4bfd4fd9e70f75fd4235f91a3a5f7e4
biglad <biglad@mgawow.co.uk>
2025-07-20 15:55:36 +0100

    Update xp_boost_spell.cpp

--- src/server/scripts/Custom/xp_boost_spell.cpp
+++ src/server/scripts/Custom/xp_boost_spell.cpp
@@ -11,22 +11,9 @@ public:
     {
         PrepareAuraScript(spell_xp_boost_aura_AuraScript);
 
-        void OnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
-        {
-            if (Unit* target = GetTarget())
-                target->ToPlayer()->SetFlag(PLAYER_FLAGS_EXTRA, PLAYER_FLAGS_EXTRA_XP_BOOST);
-        }
-
-        void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
-        {
-            if (Unit* target = GetTarget())
-                target->ToPlayer()->RemoveFlag(PLAYER_FLAGS_EXTRA, PLAYER_FLAGS_EXTRA_XP_BOOST);
-        }
-
         void Register() override
         {
-            OnEffectApply += AuraEffectApplyFn(spell_xp_boost_aura_AuraScript::OnApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
-            OnEffectRemove += AuraEffectRemoveFn(spell_xp_boost_aura_AuraScript::OnRemove, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+            // No custom handlers needed for apply/remove in 3.3.5a
         }
     };
 
