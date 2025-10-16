ba537f4c657a1278744269cd5ea4544f96d9aa63
trickerer <onlysuffering@gmail.com>
2025-10-16 16:15:02 +0700

    NPCBots: Fix a crash with Mirror Image teleport. Fix a false error during Mirror Image death in BG. Simplify delayed bots removal
    (cherry picked from AC commit 8470a2ad7fc34c4970ab6fa0c7e5793da209e019)
    
    # Conflicts:
    #	src/server/game/AI/NpcBots/botmgr.cpp

--- src/server/game/AI/NpcBots/botmgr.h
+++ src/server/game/AI/NpcBots/botmgr.h
@@ -374,7 +374,6 @@ class TC_GAME_API BotMgr
 
         Player* const _owner;
         BotMap _bots;
-        std::list<ObjectGuid> _removeList;
         std::list<std::pair<ObjectGuid, BotRemoveType>> _delayedRemoveList;
         DPSTracker* const _dpstracker;
         NpcBotMgrData* _data;
