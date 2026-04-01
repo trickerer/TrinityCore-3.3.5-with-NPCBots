8fd079909b5ab0c9d3dc37d511cd73ec90f09a92
trickerer <onlysuffering@gmail.com>
2026-04-01 18:25:05 +0700

    NPCBots: Fix clang 15 build 2. Remove appveyor and circkleci builds

--- src/server/game/AI/NpcBots/bot_ai.h
+++ src/server/game/AI/NpcBots/bot_ai.h
@@ -9,6 +9,7 @@
 #include "ItemDefines.h"
 #include "Position.h"
 
+#include <queue>
 #include <tuple>
 
 /*
@@ -23,6 +24,7 @@ enum CombatRating : uint8;
 enum EnchantmentSlot : uint16;
 enum GossipOptionIcon : uint8;
 enum MeleeHitOutcome : uint8;
+enum WeaponAttackType : uint8;
 
 struct CleanDamage;
 struct CalcDamageInfo;
